0ef0da0fd2e5a61b19615427417c9511796163a6
Pierre David <pda@unistra.fr>
2007-07-11 20:21:04 +0000

    Correction d'un bug très ancien : certains Vlans apparaissent plusieurs fois sur certains équipements.
    Le bug était dû à l'instanciation des L2pat (resp BRpat) en L2 (resp
    bridge) : cette instanciation provoquait des liens en double. La
    création des nouveaux liens lors de l'instanciation est dorénavant
    faite avec davantage de précautions.

--- anaconf/lib/l2trans.c
+++ anaconf/lib/l2trans.c
@@ -1,5 +1,5 @@
 /*
- * $Id: l2trans.c,v 1.3 2007-01-11 15:31:22 pda Exp $
+ * $Id: l2trans.c,v 1.4 2007-07-11 20:21:04 pda Exp $
  */
 
 #include "graph.h"
@@ -37,12 +37,22 @@ static int match_vlan (vlan_t v, struct vlanlist *list)
 static void transport_vlan_on_L1 (struct node *n, vlan_t v)
 {
     struct linklist *ll ;
+    int l2seen, l2pat ;
 
     if (vlan_isset (n->vlanset, v))
 	return ;
     vlan_set (n->vlanset, v) ;
 
     MK_SET (n, MK_L2TRANSPORT) ;
+
+    /*
+     * First pass : avoid L2pat nodes, since we only want to
+     * instanciate them if there is no L2 node for this vlan.
+     */
+
+    l2pat = 0 ;
+    l2seen = 0 ;
+
     for (ll = n->linklist ; ll != NULL ; ll = ll->next)
     {
 	struct link *l ;
@@ -60,9 +70,16 @@ static void transport_vlan_on_L1 (struct node *n, vlan_t v)
 		if (n->u.l1.l1type == L1T_TRUNK)
 		{
 		    if (other->u.l2.vlan == v)
+		    {
+			l2seen = 1 ;
 			transport_vlan_on_L2 (other, v) ;
+		    }
 		}
-		else transport_vlan_on_L2 (other, other->u.l2.vlan) ;
+		else
+		{
+		    l2seen = 1 ;
+		    transport_vlan_on_L2 (other, other->u.l2.vlan) ;
+		}
 		break ;
 	    case NT_L3 :
 		inconsistency ("L1-L3 : Should not happen") ;
@@ -77,6 +94,32 @@ static void transport_vlan_on_L1 (struct node *n, vlan_t v)
 		exit (2) ;
 		break ;
 	    case NT_L2PAT :
+		l2pat = 1 ;
+		break ;
+	    case NT_BRPAT :
+		inconsistency ("L1-BRPAT : Should not happen") ;
+		exit (2) ;
+		break ;
+	}
+    }
+
+    /*
+     * Second pass : if there is no L2 node for this vlan,
+     * look for an L2pat to instanciate.
+     */
+
+    if (l2pat && ! l2seen)
+    {
+	for (ll = n->linklist ; ll != NULL ; ll = ll->next)
+	{
+	    struct link *l ;
+	    struct node *other ;
+
+	    l = ll->link ;
+	    other = getlinkpeer (l, n) ;
+
+	    if (other->nodetype == NT_L2PAT)
+	    {
 		if (n->u.l1.l1type == L1T_TRUNK)
 		{
 		    if (match_vlan (v, other->u.l2pat.allowed))
@@ -87,11 +130,7 @@ static void transport_vlan_on_L1 (struct node *n, vlan_t v)
 		    inconsistency ("L1(ether)-L2PAT : Should not happen") ;
 		    exit (2) ;
 		}
-		break ;
-	    case NT_BRPAT :
-		inconsistency ("L1-BRPAT : Should not happen") ;
-		exit (2) ;
-		break ;
+	    }
 	}
     }
 }
@@ -129,7 +168,8 @@ static void transport_vlan_on_L2pat (struct node *n, vlan_t v, struct node *prev
 	    case NT_L1 :
 		if (other->u.l1.l1type == L1T_TRUNK)
 		{
-		    (void) create_link (NULL, l2node->name, other->name) ;
+		    if (other != prev)
+			(void) create_link (NULL, l2node->name, other->name) ;
 		    transport_vlan_on_L1 (other, v) ;
 		}
 		else
@@ -204,7 +244,7 @@ static void transport_vlan_on_brpat (struct node *n, vlan_t v, struct node *prev
 		exit (2) ;
 		break ;
 	    case NT_L2 :
-		if (other->u.l2.vlan == v)
+		if (other->u.l2.vlan == v && other != prev)
 		{
 		    (void) create_link (NULL, bridgenode->name, other->name) ;
 		    transport_vlan_on_L2 (other, v) ;
@@ -298,12 +338,22 @@ void transport_vlan_on_L3 (struct node *n, vlan_t v)
 void transport_vlan_on_L2 (struct node *n, vlan_t v)
 {
     struct linklist *ll ;
+    int brpat, brseen ;
 
     if (vlan_isset (n->vlanset, v))
 	return ;
     vlan_set (n->vlanset, v) ;
 
     MK_SET (n, MK_L2TRANSPORT) ;
+
+    /*
+     * First pass : avoid BRpat nodes, since we only want to
+     * instanciate them if there is no bridge node for this vlan.
+     */
+
+    brpat = 0 ;
+    brseen = 0 ;
+
     for (ll = n->linklist ; ll != NULL ; ll = ll->next)
     {
 	struct link *l ;
@@ -327,6 +377,7 @@ void transport_vlan_on_L2 (struct node *n, vlan_t v)
 		transport_vlan_on_L3 (other, v) ;
 		break ;
 	    case NT_BRIDGE :
+		brseen = 1 ;
 		transport_vlan_on_bridge (other) ;
 		break ;
 	    case NT_ROUTER :
@@ -336,15 +387,37 @@ void transport_vlan_on_L2 (struct node *n, vlan_t v)
 	    case NT_L2PAT :
 		break ;
 	    case NT_BRPAT :
+		brpat = 1 ;
+		break ;
+	}
+    }
+
+    /*
+     * Second pass : if there is no bridge node for this vlan,
+     * look for an BRpat to instanciate.
+     */
+
+    if (brpat && ! brseen)
+    {
+	for (ll = n->linklist ; ll != NULL ; ll = ll->next)
+	{
+	    struct link *l ;
+	    struct node *other ;
+
+	    l = ll->link ;
+	    other = getlinkpeer (l, n) ;
+
+	    if (other->nodetype == NT_BRPAT)
+	    {
 		/*
-		 *			     brpat
-		 *			     |      vlan transported
-		 *			     |      |  current L2
-		 *			     |      |  |
-		 *			     v      v  v
+		 *                       brpat
+		 *                       |      vlan transported
+		 *                       |      |  current L2
+		 *                       |      |  |
+		 *                       v      v  v
 		 */
 		transport_vlan_on_brpat (other, v, n) ;
-		break ;
+	    }
 	}
     }
 }
