531eee9209ded7029a18aa1ba6bad76246f5aa9a
Pierre David <pda@unistra.fr>
2007-11-27 16:19:24 +0000

    Ajout du script d'upgrade de la base.

---
+++ upgrade/13-14/upgrade.sql
@@ -0,0 +1,24 @@
+------------------------------------------------------------------------------
+-- création de la table log
+--
+-- Méthode :
+--    - modifier ce fichier pour indiquer les utilisateurs (lignes GRANT)
+--    - psql dns < upgrade.sql 
+--
+-- $Id: upgrade.sql,v 1.1 2007-11-27 16:19:24 pda Exp $
+------------------------------------------------------------------------------
+
+CREATE TABLE log (
+    date		TIMESTAMP WITHOUT TIME ZONE
+				DEFAULT CURRENT_TIMESTAMP
+				NOT NULL,
+    subsys		TEXT NOT NULL,
+    event		TEXT NOT NULL,
+    login		TEXT,
+    ip			INET,
+    msg			TEXT
+) ;
+
+GRANT ALL ON log TO dns ;
+GRANT ALL ON log TO jean ;
+GRANT ALL ON log TO pda ;
