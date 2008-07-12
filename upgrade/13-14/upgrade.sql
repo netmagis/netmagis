6771d45ec44309211f0855e008d5ce80da810baa
Jean Benoit <jean@unistra.fr>
2008-07-12 23:39:53 +0000

    ajout des profils dhcp dans les intervalles

--- upgrade/13-14/upgrade.sql
+++ upgrade/13-14/upgrade.sql
@@ -5,7 +5,7 @@
 --    - modifier ce fichier pour indiquer les utilisateurs (lignes GRANT)
 --    - psql dns < upgrade.sql 
 --
--- $Id: upgrade.sql,v 1.1 2007-11-27 16:19:24 pda Exp $
+-- $Id: upgrade.sql,v 1.2 2008-07-12 23:39:53 jean Exp $
 ------------------------------------------------------------------------------
 
 CREATE TABLE log (
@@ -22,3 +22,7 @@ CREATE TABLE log (
 GRANT ALL ON log TO dns ;
 GRANT ALL ON log TO jean ;
 GRANT ALL ON log TO pda ;
+
+ALTER TABLE dhcprange ADD COLUMN iddhcpprofil  integer ;
+ALTER TABLE dhcprange ADD CONSTRAINT iddhcpprofilfk FOREIGN KEY(iddhcpprofil) REFERENCES dhcpprofil(iddhcpprofil);
+
