d43e6f529fa046ffd420427ce81343f0a4caaad7
Pierre David <pda@unistra.fr>
2008-07-23 08:57:24 +0000

    Création du fichier.

---
+++ upgrade/14-15/upgrade.sql
@@ -0,0 +1,28 @@
+------------------------------------------------------------------------------
+-- Mise à jour de la base vers la version 1.5
+--
+--
+-- $Id: upgrade.sql,v 1.1 2008-07-23 08:57:24 pda Exp $
+------------------------------------------------------------------------------
+
+------------------------------------------------------------------------------
+-- ajout des profils dans les intervalles DHCP
+------------------------------------------------------------------------------
+
+ALTER TABLE dhcprange
+	ADD COLUMN iddhcpprofil INTEGER ;
+
+ALTER TABLE dhcprange
+	ADD CONSTRAINT iddhcpprofilfk
+	FOREIGN KEY(iddhcpprofil) REFERENCES dhcpprofil(iddhcpprofil) ;
+
+------------------------------------------------------------------------------
+-- ajout du champ "droitsmtp" dans la table RR
+------------------------------------------------------------------------------
+
+ALTER TABLE rr
+	ADD COLUMN droitsmtp BOOLEAN ;
+ALTER TABLE rr
+	ALTER COLUMN droitsmtp
+	SET DEFAULT FALSE ;
+
