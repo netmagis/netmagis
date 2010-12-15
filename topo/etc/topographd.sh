3fbf383ad29360667a62669f50bc5fea264f65bf
Pierre DAVID <pda@rancid.u-strasbg.fr>
2010-12-15 18:15:14 +0100

    Split topod into two daemons
    - toposendd send interface changes on equipments
    - topographd detect modified equipments and
        rebuild graph
    
    This split is needed to handle quickly interface
    changes, without having to wait proper graph
    rebuilding.

---
+++ topo/etc/topographd.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+#
+# Start-up script for topographd daemon
+#
+# Historique
+#   2007/07/03 : pda/jean : design
+#   2010/11/05 : pda/jean : copy from old script for the new daemon
+#   2010/12/15 : pda/jean : split for topographd
+#
+
+topographd_program=%TOPODIR%/bin/topographd
+
+case "$1" in
+        start)
+		echo -n ' topographd'
+		su rancid -c "$topographd_program &"
+		;;
+	stop)
+		/bin/kill `ps ax | grep "$topographd_program" | grep -v "grep" | cut -c1-5`
+		;;
+	restart)
+		echo "Restart topographd"
+		/bin/kill `ps ax | egrep "$topographd_program" | grep -v "grep" | cut -c1-5`
+		su rancid -c "$topographd_program &"
+		;;
+	debug)
+		shift
+		echo "Reload topographd with level $1"
+		/bin/kill `ps ax | grep $topographd_program | grep -v "grep" | cut -c1-5`
+		su rancid -c "$topographd_program -v $1 &"
+		;;
+	*)
+		echo "Usage: $0 {start | stop | restart | debug n}"
+		exit 1
+		;;
+esac
+
+exit 0
