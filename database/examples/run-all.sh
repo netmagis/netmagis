44499f9defec123f694dd10ccfabe329675a50c2
Pierre DAVID <pdagog@gmail.com>
2011-03-09 22:39:28 +0100

    welcome to import script, phase 3.
    The import script netmagis-dbimport can
    now import zones, with the syntax:
        netmagis-dbimport zone <domainname> \
    	<zonefile> <selector> <rrsupfile> <login>
    Domains and login must already exist.
    Zone prologue is read from zonefile,
    as well as zone RRs (A/AAAA and CNAME).
    
    In addition, flag "-v" can be used in all
    import operations to increment verbosity
    level.

--- database/examples/run-all.sh
+++ database/examples/run-all.sh
@@ -6,7 +6,27 @@
 #
 
 netmagis-dbcreate \
-    && netmagis-dbimport group		group.txt \
-    && netmagis-dbimport domain		domain.txt \
-    && netmagis-dbimport network 	network.txt \
+    && netmagis-dbimport -v group		group.txt \
+    && netmagis-dbimport -v domain		domain.txt \
+    && netmagis-dbimport -v network 	network.txt \
+    && netmagis-dbimport -v zone univ-machin.fr \
+			zones/univ-machin.fr univ-machin.fr \
+			/dev/null pda \
+    && netmagis-dbimport -v zone labo2.univ-machin.fr \
+			zones/labo2.univ-machin.fr labo2.univ-machin.fr \
+			/dev/null pda \
+    && netmagis-dbimport -v zone esiatf.fr \
+			zones/esiatf.fr esiatf.fr \
+			/dev/null pda \
+    && netmagis-dbimport -v zone bidon.org \
+			zones/bidon.org bidon.org \
+			/dev/null pda \
+    && netmagis-dbimport -v zone 16.172.in-addr.arpa \
+			zones/16.172.in-addr.arpa 172.16/16 \
+			/dev/null pda \
+    && netmagis-dbimport -v zone 4.3.2.1.0.6.6.0.1.0.0.2.ip6.arpa \
+			zones/4.3.2.1.0.6.6.0.1.0.0.2.ip6.arpa 2001:660:1234::/48 \
+			/dev/null pda \
     && echo "Succeeded"
+
+exit 0
