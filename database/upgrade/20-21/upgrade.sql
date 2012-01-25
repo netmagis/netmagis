1af9eb698fc18da9c849eb8a17d72399bd93fd63
Jean Benoit <jean@unistra.fr>
2012-01-25 17:57:01 +0100

    Locate Toposendd configuration commands in a database table
    cf. issue #8
    - added table topo.confcmd
    - use this table in toposendd
    - "resetvlans" action has been renamed to "resetvlan"
    admref cgi script will be adapted later

--- database/upgrade/20-21/upgrade.sql
+++ database/upgrade/20-21/upgrade.sql
@@ -14,6 +14,62 @@ pageformat	a4
 
 DROP TABLE topo.cache ;
 
+CREATE SEQUENCE topo.seq_confcmd START 1;
+
+CREATE TABLE topo.confcmd (
+    idccmd		INTEGER		-- entry id
+	    DEFAULT NEXTVAL ('topo.seq_confcmd'),
+    idtype		INTEGER,	-- equipment type
+    action		TEXT,		-- action selector : prologue, ifreset
+    rank		INTEGER,	-- sort order
+    model		TEXT,		-- regexp matching equipment model
+    command		TEXT,		-- command to send
+
+    FOREIGN KEY (idtype) REFERENCES topo.eqtype (idtype),
+    PRIMARY KEY (idccmd)
+) ;
+
+CREATE TEMPORARY TABLE tmpconfcmd (
+    type		TEXT,		-- equipment type
+    action		TEXT,		-- action selector : prologue, ifreset
+    rank		INTEGER,	-- sort order
+    model		TEXT,		-- regexp matching equipment model
+    command		TEXT		-- command to send
+) ;
+
+COPY tmpconfcmd (type, action, rank, model, command) FROM stdin ;
+cisco	prologue	100	.*	configure terminal
+cisco	ifreset	90	.*29.0.*	interface %1$s\ndefault switchport nonegotiate\ndefault switchport trunk allowed vlan\ndefault switchport trunk native vlan\ndefault switchport access vlan\ndefault switchport mode
+cisco	ifreset	100	.*	interface %1$s\nno switchport\nswitchport voice vlan none\nswitchport
+cisco	ifdisable	100	.*	interface %1$s\nshutdown
+cisco	ifenable	100	.*	interface %1$s\nno shutdown
+cisco	ifaccess	100	.*	interface %1$s\nswitchport mode access\nswitchport access vlan %2$s\nspanning-tree portfast 
+cisco	ifvoice	100	.*	interface %1$s\nswitchport voice vlan %2$s
+cisco	ifdesc	100	.*	interface %1$s\ndescription %2$s
+cisco	epilogue	100	.*	line con 0\nexit\nexit\nwrite memory 
+juniper	prologue	100	.*	configure
+juniper	ifreset	100	.*	delete interfaces %1$s unit 0 family ethernet-switching\ndelete ethernet-switching-options voip interface %1$s
+juniper	ifdisable	100	.*	set interfaces %1$s disable
+juniper	ifenable	100	.*	delete interfaces %1$s disable
+juniper	ifaccess	100	.*	set interfaces %1$s unit 0 family ethernet-switching port-mode access\nset interfaces %1$s unit 0 family ethernet-switching vlan members %2$s
+juniper	ifdesc	100	.*	set interfaces %1$s description "%2$s"
+juniper	ifvoice	100	.*	set interfaces %1$s unit 0 family ethernet-switching\nset ethernet-switching-options voip interface %1$s vlan %2$s
+juniper	epilogue	100	.*	commit\nexit configuration
+hp	prologue	100	.*	configure terminal
+hp	resetvlan	100	.*	vlan %2$s\nno tagged %1$s\nno untagged %1$s
+hp	ifenable	100	.*	interface %1$s\nenable
+hp	ifdisable	100	.*	interface %1$s\ndisable
+hp	ifaccess	100	.*	vlan %2$s\nuntagged %1$s
+hp	ifvoice	100	.*	vlan %2$s\ntagged %1$s
+hp	ifdesc	100	.*	interface %1$s\nname "%2$s"
+hp	epilogue	100	.*	vlan 1\nexit\nexit\nwrite memory
+\.
+
+INSERT INTO topo.confcmd (idtype, action, rank, model, command)
+	SELECT e.idtype, c.action, c.rank, c.model, c.command
+	FROM topo.eqtype e, tmpconfcmd c
+	WHERE e.type=c.type ;
+
 CREATE TABLE topo.dotattr (
     rank	INTEGER,	-- sort order
     type	INTEGER,	-- 2: l2, 3: l3 graph
