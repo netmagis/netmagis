96cb0e79ccc4baf2893afb506ce922ab48f04faf
Pierre DAVID <pdagog@gmail.com>
2012-04-27 14:09:21 +0200

    Fix examples.
    Commit 561669a93d32b25c6c4b5583167d680238069d38 has not
    been tested enough. At least, I should have read error
    messages...

--- database/examples/run-all.sh
+++ database/examples/run-all.sh
@@ -21,20 +21,20 @@ netmagis-dbcreate && \
     netmagis-dbimport -v zone mycompany.com \
 			zones/mycompany.com mycompany.com \
 			/dev/null pda && \
-    netmagis-dbimport -v zone plant.mycompany.com \
-			zones/plant.mycompany.com plant.mycompany.com \
+    netmagis-dbimport -v zone plant1.mycompany.com \
+			zones/plant1.mycompany.com plant1.mycompany.com \
 			/dev/null pda && \
     netmagis-dbimport -v zone subsid.co.zz \
 			zones/subsid.co.zz subsid.co.zz \
 			/dev/null pda && \
-    netmagis-dbimport -v zone myevent.org && \
-			zones/myevent.org myevent.org && \
+    netmagis-dbimport -v zone myevent.org \
+			zones/myevent.org myevent.org \
 			/dev/null pda && \
     netmagis-dbimport -v zone 16.172.in-addr.arpa \
 			zones/16.172.in-addr.arpa 172.16/16 \
 			/dev/null pda && \
-    netmagis-dbimport -v zone 4.3.2.1.0.6.6.0.1.0.0.2.ip6.arpa \
-			zones/4.3.2.1.0.6.6.0.1.0.0.2.ip6.arpa 2001:660:1234::/48 \
+    netmagis-dbimport -v zone 4.3.2.1.6.6.0.1.0.0.2.ip6.arpa \
+			zones/4.3.2.1.6.6.0.1.0.0.2.ip6.arpa 2001:660:1234::/48 \
 			/dev/null pda && \
     netmagis-dbimport -v mailrelay mailrelay.txt && \
     netmagis-dbimport -v mailrole mailrole.txt pda && \
