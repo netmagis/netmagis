76c33f2105970e28dadc565979eb3e3a9daaa32d
Pierre DAVID <pdagog@gmail.com>
2012-09-27 16:20:41 +0200

    SQL constraints on inherited SQL tables
    Close #72

--- database/upgrade/21-22/upgrade.sql
+++ database/upgrade/21-22/upgrade.sql
@@ -9,3 +9,15 @@
 DELETE FROM global.config WHERE clef = 'dnsupdateperiod' ;
 
 ALTER TABLE dns.zone RENAME COLUMN domaine TO name ;
+
+ALTER TABLE dns.zone_normale
+    ADD UNIQUE (name),
+    ADD PRIMARY KEY (idzone) ;
+
+ALTER TABLE dns.zone_reverse4
+    ADD UNIQUE (name),
+    ADD PRIMARY KEY (idzone) ;
+
+ALTER TABLE dns.zone_reverse6
+    ADD UNIQUE (name),
+    ADD PRIMARY KEY (idzone) ;
