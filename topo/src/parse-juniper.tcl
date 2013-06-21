netmagis/netmagis

commit 590bee4031fd9735cf757cb2e9fde12d4e064225

Bugfix: unmerged changes commited by error in f935220c

topo/src/parse-juniper.tcl
@@ -66,11 +66,7 @@ proc juniper-read-conf {fd} {
     while {[gets $fd ligne] > -1} {
 	regsub { ## SECRET-DATA$} $ligne {} ligne
 	# remove comment lines
-<<<<<<< HEAD
-	regsub {^#.*} $ligne {} ligne
-=======
 	regsub {^#} $ligne {} ligne
->>>>>>> 7a181f17f2bc078bb4fcdc4815987c6201b4d4aa
 
 	# join bracketed statement running on consecutive lines
 	if { [regexp {^\s*\S+\s\[[^\]]*$} $ligne]} then {

commit 9553a63117e3e3f415467095b55ce3efaa5c347c

Regression from 590bee4 & f935220

topo/src/parse-juniper.tcl
@@ -66,7 +66,7 @@ proc juniper-read-conf {fd} {
     while {[gets $fd ligne] > -1} {
 	regsub { ## SECRET-DATA$} $ligne {} ligne
 	# remove comment lines
-	regsub {^#} $ligne {} ligne
+	regsub {^#.*} $ligne {} ligne
 
 	# join bracketed statement running on consecutive lines
 	if { [regexp {^\s*\S+\s\[[^\]]*$} $ligne]} then {