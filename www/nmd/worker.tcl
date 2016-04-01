2a4cb47e46e84f4e53857892712514f3f3eb9854
Pierre DAVID <pdagog@gmail.com>
2016-04-01 17:23:01 +0200

    Split old global objects into smaller objects
    - ::local-config : access to the local configuration file
    - ::db : access to a database (to be completed)

--- www/nmd/worker.tcl
+++ www/nmd/worker.tcl
@@ -1,16 +1,6 @@
-# source %LIBNETMAGIS%
-
-# source /local/netmagis/lib/libnetmagis.tcl
-
-source ./lib.tcl
-
-set debug 1
-
 set conf(static-dir)	/tmp
-set conf(api-dir)	/local/netmagis/www/netmagis/api
-set libconf(version)	3.0.alphabidulemachin
 
-package require html
+package require snit
 package require Pgtcl
 
 array set route {
@@ -18,6 +8,44 @@ array set route {
     post {}
     put {}
     delete {}
+}
+
+##############################################################################
+# Thread initialisation
+##############################################################################
+
+proc thread-init {conffile} {
+    global dbfd
+
+    ::local-config create ::lc
+    ::lc read $conffile
+
+    ::db create ::dbdns
+    ::dbdns init "dns" ::lc 3.0.foobar 		;# "%NMVERSION%"
+
+    ::db create ::dbmac
+    ::dbmac init "mac" ::lc ""
+
+		#
+		# Access to configuration parameters (stored in the database)
+		#
+
+#		dnsconfig setdb $dbfd($db)
+
+
+    #
+    # Prepare for DB connection
+    #
+
+    set dbfd(dns) "not connected"
+    set dbfd(mac) "not connected"
+
+    #
+    # Create a global object for configuration parameters
+    #
+
+    # config ::dnsconfig
+    # puts "READY [info procs route-*]"
 }
 
 ##############################################################################
@@ -48,7 +76,6 @@ proc handle-request {uri meth parm cookie} {
 	    #
 
 	    global route
-	    global dbfd
 
 	    set found 0
 	    foreach r $route($meth) {
@@ -66,17 +93,21 @@ proc handle-request {uri meth parm cookie} {
 		    }
 		    #uplevel \#0 eval $script
 
-		    set codetext [database-reconnect dns]
-		    if {$codetext ne ""} then {
-			lassign $codetext code msg
-			::scgiapp::scgi-error $code $msg
+		    try {
+			::dbdns reconnect
+			::dbmac reconnect
+		    } on error msg {
+			::scgiapp::scgi-error 503 $msg
 		    }
 
-		    set codetext [database-reconnect mac]
-		    if {$codetext ne ""} then {
-			lassign $codetext code msg
-			::scgiapp::scgi-error $code $msg
+		    ::scgiapp::set-body "YOU KNOW WHAT? I AM HAPPY..."
+
+		    ::dbdns select "SELECT value FROM global.config WHERE
+					    key = 'schemaversion'" tab {
+			::scgiapp::set-body " WITH schemaversion = $tab(value)"
 		    }
+
+		    return
 
 		    d setdb $dbfd(dns)
 
@@ -145,162 +176,397 @@ proc handle-static {page} {
 }
 
 ##############################################################################
+# Configuration interface
+##############################################################################
+
+snit::type ::local-config {
+    variable conf
+
+    method read {file} {
+	if {[catch {set fd [open "$file" "r"]} msg]} then {
+	    error "Cannot open configuration file '$file'"
+	}
+	set lineno 1
+	set errors ""
+	set conf [dict create]
+	while {[gets $fd line] >= 0} {
+	    regsub {#.*} $line {} line
+	    regsub {\s*$} $line {} line
+	    if {$line ne ""} then {
+		if {[regexp {(\S+)\s+"(.*)"} $line m key val]} then {
+		    dict set conf $key  $val
+		} elseif {[regexp {(\S+)\s+(.*)} $line m key val]} then {
+		    dict set conf $key $val
+		} else {
+		    append errors "$file($lineno): unrecognized line $line\n"
+		}
+	    }
+	    incr lineno
+	}
+	close $fd
+	if {$errors ne ""} then {
+	    error $errors
+	}
+	dict set conf _conffile $file
+	dict set conf _version "%NMVERSION%"
+    }
+
+    method get {key} {
+	if {[dict exists $conf $key]} then {
+	    set v [dict get $conf $key]
+	} else {
+	    set v ""
+	}
+	return $v
+    }
+}
+
+##############################################################################
+# Configuration parameters from database
+##############################################################################
+
+#
+# Configuration parameters class
+#
+# This class is a simple way to access to configuration parameters
+# of the Netmagis application.
+#
+# Methods:
+# - setdb dbfd
+#	set the database handle used to access parameters
+# - class
+#	returns all known classes
+# - desc class-or-key
+#	returns the description associated with class or key
+# - keys [ class ]
+#	returns all keys associed with the class, or all known keys
+# - keytype key
+#	returns type of a given key, under the format {string|bool|text|menu x}
+#	X is present only for the "menu" type.
+# - keyhelp key
+#	returns the help message associated with a key
+# - get key
+#	returns the value associated with a key
+# - set key val
+#	set the value associated with a key and returns an empty string or
+#	an error message.
+#
+
+snit::type ::config {
+    # database object
+    variable dbo ""
+
+    # configuration parameter specification
+    # {{class class-spec} {class class-spec} ...}
+    # class = class name
+    # class-spec = {{key ro/rw type} {key ro/rw type} ...}
+    variable configspec {
+	{general
+	    {datefmt rw {string}}
+	    {dayfmt rw {string}}
+	    {authmethod rw {menu {{pgsql Internal} {ldap {LDAP}} {casldap CAS}}}}
+	    {authexpire rw {string}}
+	    {authtoklen rw {string}}
+	    {wtmpexpire rw {string}}
+	    {failloginthreshold1 rw {string}}
+	    {faillogindelay1 rw {string}}
+	    {failloginthreshold2 rw {string}}
+	    {faillogindelay2 rw {string}}
+	    {failipthreshold1 rw {string}}
+	    {failipdelay1 rw {string}}
+	    {failipthreshold2 rw {string}}
+	    {failipdelay2 rw {string}}
+	    {pageformat rw {menu {{a4 A4} {letter Letter}}} }
+	    {schemaversion ro {string}}
+	}
+	{dns
+	    {defuser rw {string}}
+	}
+	{dhcp
+	    {dhcpdefdomain rw {string}}
+	    {dhcpdefdnslist rw {string}}
+	    {default_lease_time rw {string}}
+	    {max_lease_time rw {string}}
+	    {min_lease_time rw {string}}
+	}
+	{topo
+	    {topoactive rw {bool}}
+	    {defdomain rw {string}}
+	    {topofrom rw {string}}
+	    {topoto rw {string}}
+	    {topographddelay rw {string}}
+	    {toposendddelay rw {string}}
+	    {topomaxstatus rw {string}}
+	    {sensorexpire rw {string}}
+	    {modeqexpire rw {string}}
+	    {ifchangeexpire rw {string}}
+	    {fullrancidmin rw {string}}
+	    {fullrancidmax rw {string}}
+	}
+	{mac
+	    {macactive rw {bool}}
+	}
+	{authcas
+	    {casurl rw {string}}
+	}
+	{authldap
+	    {ldapurl rw {string}}
+	    {ldapbinddn rw {string}}
+	    {ldapbindpw rw {string}}
+	    {ldapbasedn rw {string}}
+	    {ldapsearchlogin rw {string}}
+	    {ldapattrlogin rw {string}}
+	    {ldapattrname rw {string}}
+	    {ldapattrgivenname rw {string}}
+	    {ldapattrmail rw {string}}
+	    {ldapattrphone rw {string}}
+	    {ldapattrmobile rw {string}}
+	    {ldapattrfax rw {string}}
+	    {ldapattraddr rw {string}}
+	}
+	{authpgsql
+	    {authpgminpwlen rw {string}}
+	    {authpgmaxpwlen rw {string}}
+	    {authpgmailfrom rw {string}}
+	    {authpgmailreplyto rw {string}}
+	    {authpgmailcc rw {string}}
+	    {authpgmailbcc rw {string}}
+	    {authpgmailsubject rw {string}}
+	    {authpgmailbody rw {text}}
+	    {authpggroupes rw {string}}
+	}
+    }
+
+    #
+    # Internal representation of parameter specification
+    #
+    # (class)			{<cl1> ... <cln>}
+    # (class:<cl1>)		{<k1> ... <kn>}
+    # (key:<k1>:type)		{string|bool|text|menu ...}
+    # (key:<k1>:rw)		ro|rw
+    #
+
+    variable internal -array {}
+
+    constructor {} {
+	set internal(class) {}
+	foreach class $configspec {
+
+	    set classname [lindex $class 0]
+	    lappend internal(class) $classname
+	    set internal(class:$classname) {}
+
+	    foreach key [lreplace $class 0 0] {
+		lassign $key keyname keyrw keytype
+
+		lappend internal(class:$classname) $keyname
+		set internal(key:$keyname:type) $keytype
+		set internal(key:$keyname:rw) $keyrw
+	    }
+	}
+    }
+
+    method setdb {dbo} {
+	set dbo $dbo
+    }
+
+    # returns all classes
+    method class {} {
+	return $internal(class)
+    }
+
+    # returns textual description of the given class or key
+    method desc {cork} {
+	set r $cork
+	if {[info exists internal(class:$cork)]} then {
+	    set r [mc "cfg:$cork"]
+	} elseif {[info exists internal(key:$cork:type)]} {
+	    set r [mc "cfg:$cork:desc"]
+	}
+	return $r
+    }
+
+    # returns all keys associated with a class (default  : all classes)
+    method keys {{class {}}} {
+	if {[llength $class] == 0} then {
+	    set class $internal(class)
+	}
+	set lk {}
+	foreach c $class {
+	    set lk [concat $lk $internal(class:$c)]
+	}
+	return $lk
+    }
+
+    # returns key rw/ro
+    method keyrw {key} {
+	set r ""
+	if {[info exists internal(key:$key:rw)]} then {
+	    set r $internal(key:$key:rw)
+	}
+	return $r
+    }
+
+    # returns key type
+    method keytype {key} {
+	set r ""
+	if {[info exists internal(key:$key:type)]} then {
+	    set r $internal(key:$key:type)
+	}
+	return $r
+    }
+
+    # returns key help
+    method keyhelp {key} {
+	set r $key
+	if {[info exists internal(key:$key:type)]} then {
+	    set r [mc "cfg:$key:help"]
+	}
+	return $r
+    }
+
+    # returns key value
+    method get {key} {
+	if {[info exists internal(key:$key:type)]} then {
+	    set found 0
+	    $dbo select "SELECT * FROM global.config WHERE key = '$key'" tab {
+		set val $tab(value)
+		set found 1
+	    }
+	    if {! $found} then {
+		switch $internal(key:$key:type) {
+		    string	{ set val "" }
+		    bool	{ set val 0 }
+		    set text	{ set val "" }
+		    set menu	{ set val "" }
+		    default	{ set val "type unknown" }
+		}
+	    }
+	} else {
+	    error [mc "Unknown configuration key '%s'" $key]
+	}
+	return $val
+    }
+
+    # set key value
+    # returns empty string if ok, or an error message
+    method set {key val} {
+	if {[info exists internal(key:$key:rw)]} then {
+	    if {$internal(key:$key:rw) eq "rw"} then {
+		set r ""
+		set k [pg_quote $key]
+		set sql "DELETE FROM global.config WHERE key = $k"
+		if {[$dbo exec $sql msg]} then {
+		    set v [pg_quote $val]
+		    set sql "INSERT INTO global.config (key, value)
+		    				VALUES ($k, $v)"
+		    if {! [::pgsql::execsql $db $sql msg]} then {
+			set r [mc {Cannot set key '%1$s' to '%2$s': %3$s} $key $val $msg]
+		    }
+		} else {
+		    set r [mc {Cannot fetch key '%1$s': %2$s} $key $msg]
+		}
+	    } else {
+		set r [mc {Cannot modify read-only key '%s'} $key]
+	    }
+	} else {
+	    error [mc "Unknown configuration key '%s'" $key]
+	}
+
+	return $r
+    }
+}
+
+
+##############################################################################
 # PostgreSQL interface
 ##############################################################################
 
+snit::type ::db {
+    # Database prefix in configuration file
+    variable dbprefix
+
+    # Access to local configuration file
+    variable lc
+
+    # Database handler (result of pg_connect)
+    variable dbfd "not connected"
+
+    # Database handler (result of pg_connect)
+    variable dbversion
+
+    # prefix: "dns" or "mac" (according to local configuration file)
+    # confobj: access to the local configuration file
+    # versioncheck: version number (such as 3.0.5beta1) or ""
+
+    method init {prefix confobj versioncheck} {
+	set dbprefix $prefix
+	set lc $confobj
+	set dbversion $versioncheck
+    }
+
+    method disconnect {} {
+	catch {pg_disconnect $dbfd}
+	set dbfd "not connected"
+    }
+
+    method reconnect {} {
+	if {$dbfd ne "not connected"} then {
+	    return {}
+	}
+
+	#
+	# Get conninfo string
+	#
+	set conninfo {}
+	foreach f {{host host} {port port} {dbname name}
+			    {user user} {password password}} {
+	    lassign $f connkey suffix
+	    set v [$lc get "${dbprefix}db${suffix}"]
+	    regsub {['\\]} $v {\\&} v
+	    lappend conninfo "$connkey='$v'"
+	}
+	set conninfo [join $conninfo " "]
+
+	puts "conninfo=$conninfo"
+
+	try {
+	    set dbfd [pg_connect -conninfo $conninfo]
+	} on error msg {
+	    error "Database $dbprefix unavailable"
+	}
+
+#		#
+#		# Log initialization
+#		#
 #
-# Input:
-#   - db: "dns" or "mac"
-#   - sql: sql command to send to the database
-#   - tabname: name of an array used in the tcl script
-#   - script: tcl script to execute for each row
-# Output:
-#   - return value: empty string (no error) or sql error message
-#   - script error: a Tcl error is throwed
-#
+#		set log [::webapp::log create %AUTO% \
+#					    -subsys netmagis \
+#					    -method opened-postgresql \
+#					    -medium [list "db" $dbfd($db) table global.log] \
+#				]
 
-proc nm_pg_select {db sql tabname script} {
-    global dbfd
-    global debug
+    }
 
-    set r ""
-    try {
-	uplevel 1 [list pg_execute -array $tabname $dbfd($db) $sql $script]
-    } trap {NONE} {msg err} {
-	# Pgtcl 1.9 returns errorcode == NONE
-	set errinfo [dict get $err -errorinfo]
-	if {[regexp "^PGRES_FATAL_ERROR" $errinfo]} then {
-	    # reset db handle
-	    set info [pg_dbinfo status $dbfd($db)]
-	    if {$info ne "connection_ok"} then {
-		database-disconnect $db
-	    }
-
-	    # return a one-line message, or throw an error with the full stack?
-	    if {$debug} then {
-		error $msg $errinfo NONE
+    method select {sql tabname script} {
+	try {
+	    uplevel 1 [list pg_execute -array $tabname $dbfd $sql $script]
+	} trap {NONE} {msg err} {
+	    # Pgtcl 1.9 returns errorcode == NONE
+	    set errinfo [dict get $err -errorinfo]
+	    if {[regexp "^PGRES_FATAL_ERROR" $errinfo]} then {
+		# reset db handle
+		set info [pg_dbinfo status $dbfd]
+		if {$info ne "connection_ok"} then {
+		    $self disconnect
+		}
+		error $msg
 	    } else {
-		set first [lindex [split $errinfo "\n"] 0]
-		regsub {.*ERROR:\s+} $first {} r
-	    }
-	} else {
-	    # it is not a Pgtcl error
-	    error $msg $errinfo NONE
-	}
-    }
-    return $r
-}
-
-proc handle-api-v1 {sock action headers body} {
-
-    set codetext [database-reconnect dns]
-    if {$codetext ne ""} then {
-	lassign $codetext code msg
-	::scgiapp::scgi-error $code $msg
-    }
-
-    set codetext [database-reconnect mac]
-    if {$codetext ne ""} then {
-	lassign $codetext code msg
-	::scgiapp::scgi-error $code $msg
-    }
-
-#				array set h $headers
-#				parray h
-
-    #
-    # Look for form value for "name"
-    #
-    set name ""
-    if {$body ne ""} then {
-	############## XXXXXXX : improve decoding, make it robust
-	foreach pair [split $body &] {
-	    lassign [split $pair =] key val
-	    if {$key == "name"} then {
-		set name $val
-		break
+		# it is not a Pgtcl error
+		error $msg $errinfo NONE
 	    }
 	}
     }
-
-    # exec sleep 10
-
-    if {$name eq ""} then {
-	puts $sock "Status: 200 OK"
-	puts $sock "Content-Type: text/html"
-	puts $sock ""
-	puts $sock "<HTML>"
-	puts $sock "<BODY>"
-	puts $sock {<FORM METHOD="POST" ACTION="/sdshdsqghd">}
-	puts $sock {Name&nbsp;:}
-	puts $sock {<INPUT TYPE="TEXT" NAME="name">}
-	puts $sock {<INPUT TYPE="SUBMIT" VALUE ="Consult">}
-	puts $sock {</FORM>}
-	puts $sock {</BODY>}
-	puts $sock {</HTML>}
-    } else {
-	puts $sock "Status: 200 OK"
-	puts $sock "Content-Type: text/html"
-	puts $sock ""
-	puts $sock "<HTML>"
-	puts $sock "<BODY>"
-	puts $sock {<TABLE>}
-	set sql "SELECT r.name || '.' || d.name AS name,
-				i.addr,
-				v.name AS view
-			    FROM dns.rr_ip i,
-				dns.rr r,
-				dns.domain d,
-				dns.view v
-			    WHERE r.name = '$name'
-				AND r.iddom = d.iddom
-				AND r.idrr = i.idrr
-				AND r.idview = v.idview
-			    "
-	puts stdout "AVANT PGEXEC"
-	set msg [nm_pg_select dns $sql t {
-	    puts $sock {<TR>}
-	    puts $sock "<TD>$t(name)</TD>"
-	    puts $sock "<TD>$t(addr)</TD>"
-	    puts $sock "<TD>$t(view)</TD>"
-	    puts $sock {</TR>}
-	} ]
-	puts stdout "APRES PGEXEC : msg=$msg"
-	puts $sock {</TABLE>}
-	puts $sock {</BODY>}
-	puts $sock {</HTML>}
-    }
-}
-
-##############################################################################
-# Thread initialisation
-##############################################################################
-
-proc thread-init {} {
-    global dbfd
-    global conf
-
-    #
-    # Prepare for DB connection
-    #
-
-    set dbfd(dns) "not connected"
-    set dbfd(mac) "not connected"
-
-    #
-    # Create a global object for configuration parameters
-    #
-
-    config ::dnsconfig
-
-    puts "READY [info procs route-*]"
-}
-
-proc database-disconnect {db} {
-    global dbfd
-
-    catch {pg_disconnect $dbfd($db)}
-    set dbfd($db) "not connected"
 }
 
 proc api-handler {method pathspec authneeded paramspec script} {
@@ -384,92 +650,9 @@ api-handler get {/names/([0-9]+:idrr)} yes {
     }
 }
 
-#
-# Input:
-#   - db: "dns" or "mac"
-#
-
-proc database-reconnect {db} {
-    global dbfd
-    global libconf
-
-    if {$dbfd($db) ne "not connected"} then {
-	return {}
-    }
-    puts stderr "RECONNECT $db"
-
-    switch $db {
-	dns {
-	    #
-	    # Access to Netmagis database
-	    #
-
-	    set conninfo [get-conninfo "dnsdb"]
-	    if {[catch {set dbfd($db) [pg_connect -conninfo $conninfo]} msg]} then {
-		return {503 {Database unavailable}}
-	    }
-
-	    #
-	    # Access to configuration parameters (stored in the database)
-	    #
-
-	    dnsconfig setdb $dbfd($db)
-
-	    #
-	    # Check compatibility with database schema version
-	    # - empty string : pre-2.2 schema
-	    # - non empty string : integer containing schema version
-	    # Netmagis version (x.y.... => xy) must match schema version.
-	    #
-
-	    # get code version (from top-level Makefile)
-	    if {! [regsub {^(\d+)\.(\d+).*} $libconf(version) {\1\2} nver]} then {
-		set msg [format "Internal Server Error (Netmagis version number '%s' unrecognized)" $libconf(version)]
-		return [list 500 $msg]
-	    }
-
-	    # get schema version (from database)
-	    if {[catch {dnsconfig get "schemaversion"} sver]} then {
-		set sver ""
-	    }
-
-	    set msg ""
-	    if {$sver eq ""} then {
-		set msg [format "Internal Server Error (Database schema is too old. See http://netmagis.org/upgrade.html)" $version]
-	    } elseif {$sver < $nver} then {
-		set msg [format "Internal Server Error (Database schema is too old. See http://netmagis.org/upgrade.html)" $version]
-	    } elseif {$sver > $nver} then {
-		set msg [format {Internal Server Error (Database schema '%1$s' is not yet recognized by Netmagis %2$s)} $sver $version]
-	    }
-
-	    if {$msg ne ""} then {
-		return [list 500 $msg]
-	    }
-
-	    #
-	    # Log initialization
-	    #
-
-	    set log [::webapp::log create %AUTO% \
-					-subsys netmagis \
-					-method opened-postgresql \
-					-medium [list "db" $dbfd($db) table global.log] \
-			    ]
-	}
-	mac {
-	    #
-	    # Access to MAC database
-	    #
-
-	    set conninfo [get-conninfo "macdb"]
-	    if {[catch {set dbfd($db) [pg_connect -conninfo $conninfo]} msg]} then {
-		return {503 {Database unavailable}}
-	    }
-
-	}
-    }
-
-    return ""
+try {
+    thread-init $conffile
+} on error msg {
+    puts stderr $msg
+    exit 1
 }
-
-thread-init
