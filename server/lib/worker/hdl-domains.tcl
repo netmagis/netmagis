52845df4b9bdfba5a49d47f1dd5500aa1f4755cd
Pierre DAVID <pdagog@gmail.com>
2016-06-13 14:20:23 +0200

    Fix /domains and add mailrole selector

--- server/lib/worker/hdl-domains.tcl
+++ server/lib/worker/hdl-domains.tcl
@@ -1,13 +1,20 @@
 ##############################################################################
 
 api-handler get {/domains} yes {
+	mailrole 0
     } {
     set idgrp [::u idgrp]
+    set w [domains-test-mailrole $mailrole]
+    if {$w ne ""} then {
+	set w "AND $w"
+    }
+
     set sql "SELECT json_agg (t.*) AS j FROM (
-		SELECT d.*
+		SELECT d.*, p.mailrole
 		    FROM dns.domain d
 			INNER JOIN dns.p_dom p USING (iddom)
 		    WHERE p.idgrp = $idgrp
+			$w
 		    ORDER BY p.sort ASC
 		) AS t
 		"
@@ -15,6 +22,7 @@ api-handler get {/domains} yes {
     ::dbdns exec $sql tab {
 	set j $tab(j)
     }
+
     ::scgi::set-header Content-Type application/json
     ::scgi::set-body $j
 }
@@ -22,14 +30,20 @@ api-handler get {/domains} yes {
 ##############################################################################
 
 api-handler get {/domains/([0-9]+:iddom)} yes {
+	mailrole 0
     } {
     set idgrp [::u idgrp]
-    set sql "SELECT json_agg (t.*) AS j FROM (
-		SELECT d.*
+    set w [domains-test-mailrole $mailrole]
+    if {$w ne ""} then {
+	set w "AND $w"
+    }
+    set sql "SELECT row_to_json (t.*) AS j FROM (
+		SELECT d.*, p.mailrole
 		    FROM dns.domain d
 			INNER JOIN dns.p_dom p USING (iddom)
 		    WHERE p.idgrp = $idgrp
 			AND d.iddom = $iddom
+			$w
 		) AS t
 		"
     set found 0
@@ -39,9 +53,24 @@ api-handler get {/domains/([0-9]+:iddom)} yes {
     }
 
     if {! $found} then {
-	::scgi::serror 404 [mc "Domain %s not found"]
+	::scgi::serror 404 [mc "Domain %s not found" $iddom]
     }
 
     ::scgi::set-header Content-Type application/json
     ::scgi::set-body $j
+}
+
+##############################################################################
+# Utility function
+##############################################################################
+
+proc domains-test-mailrole {mailrole} {
+    switch -exact -- $mailrole {
+	{} { set w "" }
+	0  { set w "mailrole = 0" }
+	1  { set w "mailrole != 0" }
+	default {
+	    ::scgi::serror 412 [mc "Invalid mailrole value '%s' $mailrole]
+	}
+    }
 }
