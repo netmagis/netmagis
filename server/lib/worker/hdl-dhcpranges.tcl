22bfe7ad7fe1d375da23ba68d48f070670cb8a22
Pierre DAVID <pdagog@gmail.com>
2016-07-03 11:25:18 +0200

    Be stricter about DHCP ranges

--- server/lib/worker/hdl-dhcpranges.tcl
+++ server/lib/worker/hdl-dhcpranges.tcl
@@ -7,15 +7,48 @@ api-handler get {/dhcpranges} yes {
     set qcidr [pg_quote $cidr]
     set idgrp [::u idgrp]
 
+    #
+    # In order to be viewed, a DHCP range must be:
+    # - inside the given CIDR
+    # - the given CIDR itself must be inside a DHCP-enabled network
+    #	which is allowed for our group
+    # In order to be "editable", in addition to previous conditions,
+    # a DHCP range must:
+    # - reference an allowed domain
+    # - reference an allowed DHCP profile (or not reference any DHCP profile)
+    # - have all its addresses (between min and max) as allowed
+    #
+
     set sql "SELECT COALESCE (json_agg (t), '\[\]') AS j FROM (
-		SELECT d.*
+		SELECT d.iddhcprange, d.min, d.max, d.iddom,
+			dom.name AS domain,
+			d.default_lease_time, d.max_lease_time,
+			d.iddhcpprof, dh.name AS dhcpprofile,
+			BOOL_AND (pd.iddom IS NOT NULL
+				    AND pip.allow_deny = 1
+				    AND (d.iddhcpprof IS NULL
+					OR pdh.iddhcpprof IS NOT NULL)
+				    ) AS editable
 		    FROM dns.dhcprange d
-			INNER JOIN dns.p_dom p USING (iddom)
-		    WHERE min <<= $qcidr
-			AND max <<= $qcidr
-			AND p.idgrp = $idgrp
-			AND dns.check_dhcprange_grp ($idgrp, min, max)
-		    ORDER BY min
+			INNER JOIN dns.domain dom USING (iddom)
+			INNER JOIN dns.network n
+			    ON ($qcidr <<= n.addr4 AND n.dhcp != 0)
+			LEFT OUTER JOIN dns.p_dom pd
+			    ON (pd.idgrp = $idgrp AND pd.iddom = d.iddom)
+			LEFT OUTER JOIN dns.dhcpprofile dh USING (iddhcpprof)
+			LEFT OUTER JOIN dns.p_dhcpprofile pdh
+			    ON (pdh.idgrp = $idgrp
+				AND pdh.iddhcpprof = d.iddhcpprof)
+			INNER JOIN dns.p_ip pip
+			    ON (pip.idgrp = $idgrp
+			    -- select IP permissions overlapping this range
+			    AND NOT (host (broadcast (pip.addr))::inet < d.min
+				    OR host (network (pip.addr))::inet > d.max)
+			    )
+		    GROUP BY d.iddhcprange, d.min, d.max, d.iddom, dom.name,
+			d.default_lease_time, d.max_lease_time,
+			d.iddhcpprof, dh.name
+		    ORDER BY d.min
 		) AS t
 		"
     ::dbdns exec $sql tab {
@@ -62,19 +95,38 @@ api-handler delete {/dhcpranges/([0-9]+:iddhcprange)} yes {
     } {
     set idgrp [::u idgrp]
 
-    set sql "SELECT iddhcprange
-		    FROM dns.dhcprange
-			INNER JOIN dns.p_dom p USING (iddom)
+    set sql "SELECT d.iddhcprange, BOOL_AND (pd.iddom IS NOT NULL
+				    AND pip.allow_deny = 1
+				    AND (d.iddhcpprof IS NULL
+					OR pdh.iddhcpprof IS NOT NULL)
+				    ) AS editable
+		    FROM dns.dhcprange d
+			INNER JOIN dns.network n
+			    ON (d.min <<= n.addr4
+				    AND d.max <<= n.addr4
+				    AND n.dhcp != 0)
+			LEFT OUTER JOIN dns.p_dom pd
+			    ON (pd.idgrp = $idgrp AND pd.iddom = d.iddom)
+			LEFT OUTER JOIN dns.p_dhcpprofile pdh
+			    ON (pdh.idgrp = $idgrp
+				AND pdh.iddhcpprof = d.iddhcpprof)
+			INNER JOIN dns.p_ip pip
+			    ON (pip.idgrp = $idgrp
+			    -- select IP permissions overlapping this range
+			    AND NOT (host (broadcast (pip.addr))::inet < d.min
+				    OR host (network (pip.addr))::inet > d.max)
+			    )
 		    WHERE iddhcprange = $iddhcprange
-			AND p.idgrp = $idgrp
-			AND dns.check_dhcprange_grp ($idgrp, min, max)
+		    GROUP BY d.iddhcprange
 		"
-    set found 0
+    set editable 0
     ::dbdns exec $sql tab {
-	set found 1
+	if {$tab(editable) eq "t"} then {
+	    set editable 1
+	}
     }
-    if {! $found} then {
-	::scgi::serror 404 [mc "DHCP range not found"]
+    if {! $editable} then {
+	::scgi::serror 404 [mc "DHCP range not found or unauthorized"]
     }
 
     set sql "DELETE FROM dns.dhcprange WHERE iddhcprange = $iddhcprange"
@@ -90,10 +142,36 @@ api-handler delete {/dhcpranges/([0-9]+:iddhcprange)} yes {
 
 proc dhcp-get {iddhcprange idgrp} {
     set sql "SELECT row_to_json (t.*) AS j FROM (
-		SELECT *
-		    FROM dns.dhcprange
-		    WHERE iddhcprange = $iddhcprange
-			AND dns.check_dhcprange_grp ($idgrp, min, max)
+		SELECT d.iddhcprange, d.min, d.max, d.iddom,
+			dom.name AS domain,
+			d.default_lease_time, d.max_lease_time,
+			d.iddhcpprof, dh.name AS dhcpprofile,
+			BOOL_AND (pd.iddom IS NOT NULL
+				    AND pip.allow_deny = 1
+				    AND (d.iddhcpprof IS NULL
+					OR pdh.iddhcpprof IS NOT NULL)
+				    ) AS editable
+		    FROM dns.dhcprange d
+			INNER JOIN dns.domain dom USING (iddom)
+			INNER JOIN dns.network n
+			    ON (d.min <<= n.addr4 AND d.max <<= n.addr4
+				AND n.dhcp != 0)
+			LEFT OUTER JOIN dns.p_dom pd
+			    ON (pd.idgrp = $idgrp AND pd.iddom = d.iddom)
+			LEFT OUTER JOIN dns.dhcpprofile dh USING (iddhcpprof)
+			LEFT OUTER JOIN dns.p_dhcpprofile pdh
+			    ON (pdh.idgrp = $idgrp
+				AND pdh.iddhcpprof = d.iddhcpprof)
+			INNER JOIN dns.p_ip pip
+			    ON (pip.idgrp = $idgrp
+			    -- select IP permissions overlapping this range
+			    AND NOT (host (broadcast (pip.addr))::inet < d.min
+				    OR host (network (pip.addr))::inet > d.max)
+			    )
+		    WHERE d.iddhcprange = $iddhcprange
+		    GROUP BY d.iddhcprange, d.min, d.max, d.iddom, dom.name,
+			d.default_lease_time, d.max_lease_time,
+			d.iddhcpprof, dh.name
 		) AS t
 		"
     set found 0
@@ -102,7 +180,7 @@ proc dhcp-get {iddhcprange idgrp} {
 	set found 1
     }
     if {! $found} then {
-	::scgi::serror 404 [mc "DHCP range not found"]
+	::scgi::serror 404 [mc "DHCP range not found or unauthorized"]
     }
     return $j
 }
@@ -138,6 +216,26 @@ proc dhcp-new {iddhcprange idgrp _parm} {
     }
 
     #
+    # Check dhcpprofile
+    #
+
+    if {$iddhcpprof == -1} then {
+	# for SQL insert/update below
+	set iddhcpprof "NULL"
+    } else {
+	set sql "SELECT iddhcpprof
+		    FROM dns.p_dhcpprofile
+		    WHERE iddhcpprof = $iddhcpprof"
+	set found 0
+	::dbdns exec $sql tab {
+	    set found 1
+	}
+	if {! $found} then {
+	    ::scgi::serror 412 [mc {Invalid profile}]
+	}
+    }
+
+    #
     # Check *_lease_time values
     #
 
@@ -147,26 +245,6 @@ proc dhcp-new {iddhcprange idgrp _parm} {
 
     if {$max_lease_time != 0 && $max_lease_time < $min_lease_time} then {
 	::scgi::serror 404 [mc "Max_lease_time value less than '%s'" $min_lease_time]
-    }
-
-    #
-    # Check dhcpprofile
-    #
-
-    if {$iddhcpprof == -1} then {
-	# for SQL insert/update below
-	set iddhcpprof "NULL"
-    } else {
-	set sql "SELECT iddhcpprof
-		    FROM dns.dhcpprofile
-		    WHERE iddhcpprof = $iddhcpprof"
-	set found 0
-	::dbdns exec $sql tab {
-	    set found 1
-	}
-	if {! $found} then {
-	    ::scgi::serror 412 [mc {Invalid profile}]
-	}
     }
 
     #
@@ -185,21 +263,44 @@ proc dhcp-new {iddhcprange idgrp _parm} {
     }
 
     #
-    # Check address ownership
+    # Check network ownership
     #
 
     set sql "SELECT count(*) AS c
 		FROM dns.network n
 		    INNER JOIN dns.p_network p USING (idnet)
 		WHERE p.idgrp = $idgrp
-		    AND n.addr4 >>= (inet $qmin)
-		    AND n.addr4 >>= (inet $qmax)
+		    AND n.dhcp != 0
+		    AND (inet $qmin) <<= n.addr4
+		    AND (inet $qmax) <<= n.addr4
 	    "
     ::dbdns exec $sql tab {
 	set c $tab(c)
     }
     if {$c == 0} then {
-	::scgi::serror 412 [mc {Range (%1$s,%2$s) is not in an allowed network} $min $max]
+	::scgi::serror 412 [mc {Range (%1$s...%2$s) is not in an allowed network} $min $max]
+    }
+
+    #
+    # Check individual addresses permissions
+    #
+
+    set sql "SELECT BOOL_AND (allow_deny = 1) AS editable
+		FROM dns.p_ip
+		WHERE idgrp = $idgrp
+		    AND NOT (host (broadcast (addr))::inet < (inet $qmin)
+			    OR host (network (addr))::inet > (inet $qmax))
+	    "
+    set editable 0
+    ::dbdns exec $sql tab {
+	switch $tab(editable) {
+	    {}  { set editable 0 }
+	    {t} { set editable 1 }
+	    {f} { set editable 0 }
+	}
+    }
+    if {$editable == 0} then {
+	::scgi::serror 412 [mc {Range (%1$s...%2$s) holds unauthorized addresses} $min $max]
     }
 
     #
@@ -220,7 +321,7 @@ proc dhcp-new {iddhcprange idgrp _parm} {
 	set c $tab(c)
     }
     if {$c != 0} then {
-	::scgi::serror 412 [mc {Range (%1$s,%2$s) overlaps another range} $min $max]
+	::scgi::serror 412 [mc {Range (%1$s...%2$s) overlaps another range} $min $max]
     }
 
     #
@@ -238,7 +339,7 @@ proc dhcp-new {iddhcprange idgrp _parm} {
 	set c $tab(c)
     }
     if {$c > 0} then {
-	:scgi::serror 412 [mc {Conflict between dynamic range (%1$s) and %2$s IP address(es) declared with a MAC address} "$min...$max" $c]
+	:scgi::serror 412 [mc {Conflict between dynamic range (%1$s...%2$s) and %3$s IP address(es) declared with a MAC address} $min $max $c]
     }
 
     #
