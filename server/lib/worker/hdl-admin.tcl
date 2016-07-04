782b0c9c35471a9a67180b588c2cc1a378e0d098
Pierre DAVID <pdagog@gmail.com>
2016-07-04 17:15:23 +0200

    Document /admin API, add "delete" method and fix some bugs

--- server/lib/worker/hdl-admin.tcl
+++ server/lib/worker/hdl-admin.tcl
@@ -5,26 +5,28 @@
 #
 
 set admin_tables {
-    dns.network		{ref {idnet idorg idcomm}}
-    dns.community	{ref {idcomm}}
-    dns.organization	{ref {idorg}}
-    dns.zone_forward	{ref {idview}}
-    dns.zone_reverse4	{ref {idview}}
-    dns.zone_reverse6	{ref {idview}}
-    dns.hinfo		{ref {idhinfo}}
-    dns.domain		{ref {iddom}}
-    dns.dhcpprofile	{ref {iddhcpprof}}
-    dns.view		{ref {idview}}
-    global.config	{ref {key}}
-    global.nmuser	{ref {idcor idgrp}}
-    global.nmgroup	{ref {idgrp}}
+    dns.network		{ref {idnet int idorg int idcomm int}}
+    dns.community	{ref {idcomm int}}
+    dns.organization	{ref {idorg int}}
+    dns.zone_forward	{ref {idview int}}
+    dns.zone_reverse4	{ref {idview int}}
+    dns.zone_reverse6	{ref {idview int}}
+    dns.hinfo		{ref {idhinfo int}}
+    dns.domain		{ref {iddom int}}
+    dns.dhcpprofile	{ref {iddhcpprof int}}
+    dns.view		{ref {idview int}}
+    global.config	{ref {key str}}
+    global.nmuser	{ref {idcor int idgrp int}}
+    global.nmgroup	{ref {idgrp int}}
 
-    dns.p_ip		{gperm {idgrp}}
-    dns.p_network	{gperm {idgrp idnet}}
-    dns.p_view		{gperm {idgrp idview}}
-    dns.p_dchpprofile	{gperm {idgrp iddhcpprof}}
-    dns.p_dom		{gperm {idgrp iddom}}
+    dns.p_ip		{gperm {idgrp int}}
+    dns.p_network	{gperm {idgrp int idnet int}}
+    dns.p_view		{gperm {idgrp int idview int}}
+    dns.p_dchpprofile	{gperm {idgrp int iddhcpprof int}}
+    dns.p_dom		{gperm {idgrp int iddom int}}
 }
+
+################################################################################
 
 api-handler get {/admin/([a-z._]+:table)} yes {
     } {
@@ -45,21 +47,30 @@ api-handler get {/admin/([a-z._]+:table)} yes {
     #
 
     set lwhere {}
-    foreach id [lreplace $lid 0 0] {
+    foreach {id tp} [lreplace $lid 0 1] {
 	if {[dict exists $_parm $id]} then {
 	    set val [lindex [dict get $_parm $id] 0]
-	    if {! [regexp {^[0-9]+$} $val]} then {
-		::scgi::serror 404 [mc "%s parameter is not numeric" $id]
+	    switch $tp {
+		int {
+		    if {! [regexp {^[0-9]+} $val]} then {
+			::scgi::serror 404 [mc "invalid parameter '%s'" $id]
+		    }
+		}
+		str {
+		    set val [pg_quote $val]
+		}
 	    }
 	    lappend lwhere "$id = $val"
 	}
     }
-    set where ""
     if {[llength $lwhere] > 0} then {
 	set where [format "WHERE %s" [join $lwhere " AND "]]
     }
 
     if {$type eq "ref"} then {
+	if {[llength $lwhere] > 0} then {
+	    set where [format "WHERE %s" [join $lwhere " AND "]]
+	}
 	set idname [lindex $lid 0]
 	set sql "SELECT COALESCE (json_agg (r), '\[\]') AS j FROM (
 			SELECT * FROM $table $where
@@ -67,6 +78,9 @@ api-handler get {/admin/([a-z._]+:table)} yes {
 		"
     } else {
 	# idname is idgrp
+	if {[llength $lwhere] > 0} then {
+	    set where [format "AND %s" [join $lwhere " AND "]]
+	}
 	set sql "SELECT COALESCE (json_agg (r), '\[\]') AS j FROM (
 			SELECT g.idgrp, gp.perm
 			    FROM global.nmgroup g
@@ -75,6 +89,7 @@ api-handler get {/admin/([a-z._]+:table)} yes {
 						AS perm
 					    FROM $table p
 					    WHERE p.idgrp = g.idgrp
+						$where
 				    ) AS gp
 		    ) AS r
 		"
@@ -88,7 +103,9 @@ api-handler get {/admin/([a-z._]+:table)} yes {
     ::scgi::set-body $j
 }
 
-api-handler get {/admin/([a-z._]+:table)/([0-9]+:id)} yes {
+################################################################################
+
+api-handler get {/admin/([a-z._]+:table)/([^/]+:id)} yes {
     } {
     if {! [::u cap "p_admin"]} then {
 	::scgi::serror 403 [mc "Forbidden"]
@@ -103,6 +120,10 @@ api-handler get {/admin/([a-z._]+:table)/([0-9]+:id)} yes {
 
     if {$type eq "ref"} then {
 	set idname [lindex $lid 0]
+	set idtp [lindex $lid 1]
+	if {$idtp eq "str"} then {
+	    set id [pg_quote $id]
+	}
 	set sql "SELECT row_to_json (t.*) AS j
 			FROM $table t
 			WHERE $idname = $id
@@ -136,6 +157,41 @@ api-handler get {/admin/([a-z._]+:table)/([0-9]+:id)} yes {
     ::scgi::set-body $j
 }
 
+################################################################################
+
+api-handler delete {/admin/([a-z._]+:table)/([^/]+:id)} yes {
+    } {
+    if {! [::u cap "p_admin"]} then {
+	::scgi::serror 403 [mc "Forbidden"]
+    }
+
+    global admin_tables
+
+    if {! [dict exists $admin_tables $table]} then {
+	::scgi::serror 404 [mc "Table %s not found" $table]
+    }
+    lassign [dict get $admin_tables $table] type lid
+
+
+    if {$type ne "ref" || $type eq "global.config"} then {
+	::scgi::serror 403 [mc "Table %s not allowed" $table]
+    }
+
+    set idname [lindex $lid 0]
+    set idtp [lindex $lid 1]
+    if {$idtp eq "str"} then {
+	set id [pg_quote $id]
+    }
+
+    set sql "DELETE FROM $table t WHERE $idname = $id"
+
+    ::dbdns exec $sql
+    ::scgi::set-header Content-Type text/plain
+    ::scgi::set-body "OK"
+}
+
+################################################################################
+
 #
 # POST to enter a new item is for referential tables only (i.e. not for
 # permissions)
@@ -161,7 +217,7 @@ api-handler post {/admin/([a-z._]+:table)} yes {
     }
     lassign [dict get $admin_tables $table] type lid
 
-    if {$type ne "ref"} then {
+    if {$type ne "ref" || $type eq "global.config"} then {
 	::scgi::serror 403 [mc "Table %s not allowed" $table]
     }
     set idname [lindex $lid 0]
@@ -195,6 +251,8 @@ api-handler post {/admin/([a-z._]+:table)} yes {
     ::scgi::set-body $j
 }
 
+################################################################################
+
 #
 # Expecting:
 #	[ { attr1:val11, attr2:val12....}, {attr1:val21, attr2:val22...}... ]
@@ -205,7 +263,7 @@ api-handler post {/admin/([a-z._]+:table)} yes {
 # No check on individual attribute names, except usual identifier syntax
 #	
 
-api-handler put {/admin/([a-z._]+:table)/([0-9]+:id)} yes {
+api-handler put {/admin/([a-z._]+:table)/([^/]+:id)} yes {
     } {
     if {! [::u cap "p_admin"]} then {
 	::scgi::serror 403 [mc "Forbidden"]
@@ -223,6 +281,10 @@ api-handler put {/admin/([a-z._]+:table)/([0-9]+:id)} yes {
 
     if {$type eq "ref"} then {
 	set idname [lindex $lid 0]
+	set idtp [lindex $lid 1]
+	if {$idtp eq "str"} then {
+	    set id [pg_quote $id]
+	}
 
 	#
 	# Update referential table
@@ -240,7 +302,7 @@ api-handler put {/admin/([a-z._]+:table)/([0-9]+:id)} yes {
 	}
 	set cols [join $lcol ","]
 
-	set sql "CREATE TEMPORARY TABLE $temp ON COMMIT DROP AS
+	set sql1 "CREATE TEMPORARY TABLE $temp ON COMMIT DROP AS
 		    SELECT *
 			FROM json_populate_record (null::$table, $qbody) ;
 		UPDATE $temp SET $idname = $id ;
@@ -248,24 +310,46 @@ api-handler put {/admin/([a-z._]+:table)/([0-9]+:id)} yes {
 			SET ($cols) = (SELECT * FROM $temp)
 			WHERE $idname = $id
 		"
+
+	set sql2 "SELECT row_to_json (t.*) AS j
+			FROM $table t
+			WHERE $idname = $id
+		    "
     } else {
 	#
 	# Insert new permissions for this group
 	#
 
-	set sql "CREATE TEMPORARY TABLE $temp ON COMMIT DROP AS
+	set sql1 "CREATE TEMPORARY TABLE $temp ON COMMIT DROP AS
 		    SELECT *
 			FROM json_populate_recordset (null::$table, $qbody) ;
 		UPDATE $temp SET idgrp = $id ;
 		DELETE FROM $table WHERE idgrp = $id ;
 		INSERT INTO $table SELECT * FROM $temp
 		"
+
+	set sql2 "SELECT row_to_json (r.*) AS j
+		    FROM (
+			SELECT $id AS idgrp, gp.perm
+			    FROM (
+				SELECT array_agg (row_to_json (p.*))
+					AS perm
+				    FROM $table p
+				    WHERE p.idgrp = $id
+			    ) AS gp
+		    ) AS r
+		"
     }
 
     ::dbdns lock [list $table] {
-	::dbdns exec $sql
+	::dbdns exec $sql1
+    }
+
+
+    ::dbdns exec $sql2 tab {
+	set j $tab(j)
     }
 
     ::scgi::set-header Content-Type application/json
-    ::scgi::set-body ""
+    ::scgi::set-body $j
 }
