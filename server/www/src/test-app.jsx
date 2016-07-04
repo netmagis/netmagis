ceaf6dbbc3dacf86fc4fdaf32ff8e6659f1a5e4b
lgcon <lgcon@pinknet.u-strasbg.fr>
2016-07-04 11:44:33 +0200

    Create table for admin/dns.p_views

---
+++ server/www/src/test-app.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Translator, updateTranslations} from './lang.jsx';
+import * as F from './bootstrap-lib/form-utils.jsx';
+
+
+	
+
+var App = React.createClass({
+
+
+	/* This will force a rerendering on languae change */
+ 	contextTypes : {lang: React.PropTypes.string},
+
+	getInitialState: function (){
+		return {idgrp: 1};
+	},
+
+	model: { key: "_key",
+		 desc: [ 
+				[ "Sort class" , "Input" , "sort"],
+				[ "Name" , "Dropdown" , "view"]
+		]
+	},
+	
+	render: function () {
+		return ( 
+			<div>
+				<F.Table model={this.model} 
+					 name="allowed_views" 
+					 params={{idgrp: this.state.idgrp}}
+				/>
+			</div>
+		);
+
+	}
+});
+
+
+
+/* Rendering the app on the node with id = 'app'
+   change it in case of conflict   */
+
+var dom_node = document.getElementById('app');
+
+ReactDOM.render( <Translator> <App /> </Translator>, dom_node);
+
+
+
