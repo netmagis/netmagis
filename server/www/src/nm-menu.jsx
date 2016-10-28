f97db812d1ad706708acb36e56f5293ba41ac9a0
Pierre DAVID <pdagog@gmail.com>
2016-10-28 09:13:55 +0200

    Simplify menus by using new components

--- server/www/src/nm-menu.jsx
+++ server/www/src/nm-menu.jsx
@@ -77,253 +77,75 @@ export var NMMenu = React.createClass ({
 		      </a>
 		    </li>
 
-		    <li className={"dropdown" + this.showIf ('logged')}>
-		      <a href="#"
-			  className="dropdown-toggle"
-			  data-toggle="dropdown"
-			  role="button"
-			  aria-haspopup="true"
-			  aria-expanded="false"
-			  >
-			{S.mc ('DNS')}
-			<span className="caret" />
-		      </a>
-		      <ul className="dropdown-menu">
-			<li key="dns1">
-			  <a href="#">{S.mc ('Consult')}</a>
-			</li>
-			<li key="dns2">
-			  <a href="#">
-			    <p onClick={this.gotoApp.bind (this, 'add')}>
-			      {S.mc ('Add')}
-			    </p>
-			  </a>
-			</li>
-			<li key="dns3">
-			  <a href="#">{S.mc ('Delete')}</a>
-			</li>
-			<li key="dns4">
-			  <a href="#">{S.mc ('Modify')}</a>
-			</li>
-			<li key="dns5">
-			  <a href="#">{S.mc ('Mail roles')}</a>
-			</li>
-			<li key="dns6">
-			  <a href="#">
-			    <p onClick={this.gotoApp.bind (this, 'dhcprange')}>
-			      {S.mc ('DHCP ranges')}
-			    </p>
-			  </a>
-			</li>
-			<li key="dns7" className={this.showIf ('pgauth')}>
-			  <a href="#">{S.mc ('Password')}</a>
-			</li>
-			<li key="dns8">
-			  <a href="#">{S.mc ('Where am I?')}</a>
-			</li>
-		      </ul>
-		    </li>
+		    <NMDropdown k="dns" t={S.mc ('DNS')} show={this.showIf ('logged')}>
+		      <NMItem k="dns1" js={this.gotoApp.bind (this, 'consult')} t={S.mc ('Consult')} />
+		      <NMItem k="dns2" js={this.gotoApp.bind (this, 'add')} t={S.mc ('Add')} />
+		      <NMItem k="dns3" js={this.gotoApp.bind (this, 'del')} t={S.mc ('Delete')} />
+		      <NMItem k="dns4" js={this.gotoApp.bind (this, 'mod')} t={S.mc ('Modify')} />
+		      <NMItem k="dns5" js={this.gotoApp.bind (this, 'mailrole')} t={S.mc ('Mail roles')} />
+		      <NMItem k="dns6" js={this.gotoApp.bind (this, 'dhcprange')} t={S.mc ('DHCP ranges')} />
+		      <NMItem k="dns7" js={this.gotoApp.bind (this, 'pgpasswd')} t={S.mc ('Password')} show={this.showIf ('pgauth')} />
+		      <NMItem k="dns8" js={this.gotoApp.bind (this, 'where')} t={S.mc ('Where am I?')} />
+		    </NMDropdown>
 
-		    <li className={"dropdown" + this.showIf ('topo')}>
-		      <a href="#"
-			  className="dropdown-toggle"
-			  data-toggle="dropdown"
-			  role="button"
-			  aria-haspopup="true"
-			  aria-expanded="false"
-			  >
-			{S.mc ('Topo')}
-			<span className="caret" />
-		      </a>
-		      <ul className="dropdown-menu">
-			<li key="topo1">
-			  <a href="#">{S.mc ('Equipments')}</a>
-			</li>
-			<li key="topo2">
-			  <a href="#">{S.mc ('Vlans')}</a>
-			</li>
-			<li key="topo3">
-			  <a href="#">{S.mc ('Networks')}</a>
-			</li>
-			<li key="topo4" className={this.showIf ('genl')}>
-			  <a href="#">{S.mc ('Link number')}</a>
-			</li>
-			<li key="topo5" className={this.showIf ('admin')}>
-			  <a href="#">{S.mc ('Status')}</a>
-			</li>
-		      </ul>
-		    </li>
+		    <NMDropdown k="topo" t={S.mc ('Topo')} show={this.showIf ('topo')}>
+		      <NMItem k="topo1" js={this.gotoApp.bind (this, 'eq')} t={S.mc ('Equipments')} />
+		      <NMItem k="topo2" js={this.gotoApp.bind (this, 'l2')} t={S.mc ('Vlans')} />
+		      <NMItem k="topo3" js={this.gotoApp.bind (this, 'l3')} t={S.mc ('Networks')} />
+		      <NMItem k="topo4" js={this.gotoApp.bind (this, 'genl')} t={S.mc ('Link number')} show={this.showIf ('genl')} />
+		      <NMItem k="topo5" js={this.gotoApp.bind (this, 'topotop')} t={S.mc ('Status')} show={this.showIf ('admin')} />
+		    </NMDropdown>
 
-		    <li className={"dropdown" + this.showIf ('mac')}>
-		      <a href="#"
-			  className="dropdown-toggle"
-			  data-toggle="dropdown"
-			  role="button"
-			  aria-haspopup="true"
-			  aria-expanded="false"
-			  >
-			{S.mc ('MAC')}
-			<span className="caret" />
-		      </a>
-		      <ul className="dropdown-menu">
-			<li key="mac1">
-			  <a href="macindex">{S.mc ('Index')}</a>
-			</li>
-			<li key="mac2">
-			  <a href="mac">{S.mc ('Search')}</a>
-			</li>
-			<li key="mac3">
-			  <a href="ipinact">{S.mc ('Inactive addr.')}</a>
-			</li>
-			<li key="mac4">
-			  <a href="macstat">{S.mc ('Stats')}</a>
-			</li>
-		      </ul>
-		    </li>
+		    <NMDropdown k="mac" t={S.mc ('MAC')} show={this.showIf ('mac')}>
+		      <NMItem k="mac1" js={this.gotoApp.bind (this, 'macindex')} t={S.mc ('Index')} />
+		      <NMItem k="mac2" js={this.gotoApp.bind (this, 'mac')} t={S.mc ('Search')} />
+		      <NMItem k="mac3" js={this.gotoApp.bind (this, 'ipinact')} t={S.mc ('Inactive addr.')} />
+		      <NMItem k="mac4" js={this.gotoApp.bind (this, 'macstat')} t={S.mc ('Stats')} />
+		    </NMDropdown>
 
-		    <li className={"dropdown" + this.showIf ('admin')}>
-		      <a href="#"
-			  className="dropdown-toggle"
-			  data-toggle="dropdown"
-			  role="button"
-			  aria-haspopup="true"
-			  aria-expanded="false"
-			  >
-			{S.mc ('Admin')}
-			<span className="caret" />
-		      </a>
-		      <ul className="dropdown-menu">
-			<li key="admin1">
-			  <a href="admlmx">{S.mc ('List MX')}</a>
-			</li>
-			<li key="admin2">
-			  <a href="lnet">{S.mc ('List networks')}</a>
-			</li>
-			<li key="admin3">
-			  <a href="lusers">{S.mc ('List users')}</a>
-			</li>
-			<li key="admin4">
-			  <a href="who?action=now">{S.mc ('Connected users')}</a>
-			</li>
-			<li key="admin5">
-			  <a href="who?action=last">{S.mc ('Last connections')}</a>
-			</li>
-			<li key="admin6">
-			  <a href="#">{S.mc ('Status')}</a>
-			</li>
-			<li key="admin7">
-			  <a href="admref?type=org">{S.mc ('Modify organizations')}</a>
-			</li>
-			<li key="admin8">
-			  <a href="admref?type=comm">{S.mc ('Modify communities')}</a>
-			</li>
-			<li key="admin9">
-			  <a href="admref?type=hinfo">{S.mc ('Modify machine types')}</a>
-			</li>
-			<li key="admin10">
-			  <a href="admref?type=net">{S.mc ('Modify networks')}</a>
-			</li>
-			<li key="admin11">
-			  <a href="admref?type=domain">{S.mc ('Modify domains')}</a>
-			</li>
-			<li key="admin12">
-			  <a href="admmrel">{S.mc ('Modify mailhost')}</a>
-			</li>
-			<li key="admin13">
-			  <a href="admmx">{S.mc ('Modify MX')}</a>
-			</li>
-			<li key="admin14">
-			  <a href="admref?type=view">{S.mc ('Modify views')}</a>
-			</li>
-			<li key="admin15">
-			  <a href="admref?type=zone">{S.mc ('Modify zones')}</a>
-			</li>
-			<li key="admin16">
-			  <a href="admref?type=zone4">{S.mc ('Modify rev IPv4 zones')}</a>
-			</li>
-			<li key="admin17">
-			  <a href="admref?type=zone6">{S.mc ('Modify rev IPv6 zones')}</a>
-			</li>
-			<li key="admin18">
-			  <a href="admref?type=dhcpprof">{S.mc ('Modify DHCP profiles')}</a>
-			</li>
-			<li key="admin19">
-			  <a href="admref?type=vlan">{S.mc ('Modify Vlans')}</a>
-			</li>
-			<li key="admin20">
-			  <a href="admref?type=eqtype">{S.mc ('Modify equipment types')}</a>
-			</li>
-			<li key="admin21">
-			  <a href="admref?type=eq">{S.mc ('Modify equipments')}</a>
-			</li>
-			<li key="admin22">
-			  <a href="admref?type=confcmd">{S.mc ('Modify configuration commands')}</a>
-			</li>
-			<li key="admin23">
-			  <a href="admref?type=dotattr">{S.mc ('Modify Graphviz attributes')}</a>
-			</li>
-			<li key="admin24">
-			  <a href="admgrp">{S.mc ('Modify users and groups')}</a>
-			</li>
-			<li key="admin25">
-			  <a href="admzgen">{S.mc ('Force zone generation')}</a>
-			</li>
-			<li key="admin26">
-			  <a href="admpar">{S.mc ('Application parameters')}</a>
-			</li>
-			<li key="admin27">
-			  <a href="statuser">{S.mc ('Statistics by user')}</a>
-			</li>
-			<li key="admin28">
-			  <a href="statorg">{S.mc ('Statistics by organization')}</a>
-			</li>
-		      </ul>
-		    </li>
+		    <NMDropdown k="admin" t={S.mc ('Admin')} show={this.showIf ('admin')}>
+		      <NMItem k="admin1" js={this.gotoApp.bind (this, 'admlmx')} t={S.mc ('List MX')} />
+		      <NMItem k="admin2" js={this.gotoApp.bind (this, 'lnet')} t={S.mc ('List networks')} />
+		      <NMItem k="admin3" js={this.gotoApp.bind (this, 'lusers')} t={S.mc ('List users')} />
+		      <NMItem k="admin4" js={this.gotoApp.bind (this, 'who?action=now')} t={S.mc ('Connected users')} />
+		      <NMItem k="admin5" js={this.gotoApp.bind (this, 'who?action=last')} t={S.mc ('Last connections')} />
+		      <NMItem k="admin6" js={this.gotoApp.bind (this, '#')} t={S.mc ('Status')} />
+		      <NMItem k="admin7" js={this.gotoApp.bind (this, 'admref?type=org')} t={S.mc ('Modify organizations')} />
+		      <NMItem k="admin8" js={this.gotoApp.bind (this, 'admref?type=comm')} t={S.mc ('Modify communities')} />
+		      <NMItem k="admin9" js={this.gotoApp.bind (this, 'admref?type=hinfo')} t={S.mc ('Modify machine types')} />
+		      <NMItem k="admin10" js={this.gotoApp.bind (this, 'admref?type=net')} t={S.mc ('Modify networks')} />
+		      <NMItem k="admin11" js={this.gotoApp.bind (this, 'admref?type=domain')} t={S.mc ('Modify domains')} />
+		      <NMItem k="admin12" js={this.gotoApp.bind (this, 'admmrel')} t={S.mc ('Modify mailhost')} />
+		      <NMItem k="admin13" js={this.gotoApp.bind (this, 'admmx')} t={S.mc ('Modify MX')} />
+		      <NMItem k="admin14" js={this.gotoApp.bind (this, 'admref?type=view')} t={S.mc ('Modify views')} />
+		      <NMItem k="admin15" js={this.gotoApp.bind (this, 'admref?type=zone')} t={S.mc ('Modify zones')} />
+		      <NMItem k="admin16" js={this.gotoApp.bind (this, 'admref?type=zone4')} t={S.mc ('Modify rev IPv4 zones')} />
+		      <NMItem k="admin17" js={this.gotoApp.bind (this, 'admref?type=zone6')} t={S.mc ('Modify rev IPv6 zones')} />
+		      <NMItem k="admin18" js={this.gotoApp.bind (this, 'admref?type=dhcpprof')} t={S.mc ('Modify DHCP profiles')} />
+		      <NMItem k="admin19" js={this.gotoApp.bind (this, 'admref?type=vlan')} t={S.mc ('Modify Vlans')} />
+		      <NMItem k="admin20" js={this.gotoApp.bind (this, 'admref?type=eqtype')} t={S.mc ('Modify equipment types')} />
+		      <NMItem k="admin21" js={this.gotoApp.bind (this, 'admref?type=eq')} t={S.mc ('Modify equipments')} />
+		      <NMItem k="admin22" js={this.gotoApp.bind (this, 'admref?type=confcmd')} t={S.mc ('Modify configuration commands')} />
+		      <NMItem k="admin23" js={this.gotoApp.bind (this, 'admref?type=dotattr')} t={S.mc ('Modify Graphviz attributes')} />
+		      <NMItem k="admin24" js={this.gotoApp.bind (this, 'admgrp')} t={S.mc ('Modify users and groups')} />
+		      <NMItem k="admin25" js={this.gotoApp.bind (this, 'admzgen')} t={S.mc ('Force zone generation')} />
+		      <NMItem k="admin26" js={this.gotoApp.bind (this, 'admpar')} t={S.mc ('Application parameters')} />
+		      <NMItem k="admin27" js={this.gotoApp.bind (this, 'statuser')} t={S.mc ('Statistics by user')} />
+		      <NMItem k="admin28" js={this.gotoApp.bind (this, 'statorg')} t={S.mc ('Statistics by organization')} />
+		    </NMDropdown>
 
-		    <li className={"dropdown" + this.showIf ('pgadmin')}>
-		      <a href="#"
-			  className="dropdown-toggle"
-			  data-toggle="dropdown"
-			  role="button"
-			  aria-haspopup="true"
-			  aria-expanded="false"
-			  >
-			{S.mc ('Auth')}
-			<span className="caret" />
-		      </a>
-		      <ul className="dropdown-menu">
-			<li key="pgadmin1">
-			  <a href="pgaacc?action=list">{S.mc ('List accounts')}</a>
-			</li>
-			<li key="pgadmin2">
-			  <a href="pgaacc?action=print">{S.mc ('Print accounts')}</a>
-			</li>
-			<li key="pgadmin3">
-			  <a href="pgaacc?action=add">{S.mc ('Add account')}</a>
-			</li>
-			<li key="pgadmin4">
-			  <a href="pgaacc?action=mod">{S.mc ('Modify account')}</a>
-			</li>
-			<li key="pgadmin5">
-			  <a href="pgaacc?action=del">{S.mc ('Remove account')}</a>
-			</li>
-			<li key="pgadmin6">
-			  <a href="pgaacc?action=passwd">{S.mc ('Change account password')}</a>
-			</li>
-			<li key="pgadmin7">
-			  <a href="pgarealm?action=list">{S.mc ('List realms')}</a>
-			</li>
-			<li key="pgadmin8">
-			  <a href="pgarealm?action=add">{S.mc ('Add realm')}</a>
-			</li>
-			<li key="pgadmin9">
-			  <a href="pgarealm?action=mod">{S.mc ('Modify realm')}</a>
-			</li>
-			<li key="pgadmin10">
-			  <a href="pgarealm?action=del">{S.mc ('Remove realm')}</a>
-			</li>
-		      </ul>
-		    </li>
+		    <NMDropdown k="pgadmin" t={S.mc ('Auth')} show={this.showIf ('pgadmin')}>
+		      <NMItem k="pgadmin1" js={this.gotoApp.bind (this, 'pgaacc?action=list')} t={S.mc ('List accounts')} />
+		      <NMItem k="pgadmin2" js={this.gotoApp.bind (this, 'pgaacc?action=print')} t={S.mc ('Print accounts')} />
+		      <NMItem k="pgadmin3" js={this.gotoApp.bind (this, 'pgaacc?action=add')} t={S.mc ('Add account')} />
+		      <NMItem k="pgadmin4" js={this.gotoApp.bind (this, 'pgaacc?action=mod')} t={S.mc ('Modify account')} />
+		      <NMItem k="pgadmin5" js={this.gotoApp.bind (this, 'pgaacc?action=del')} t={S.mc ('Remove account')} />
+		      <NMItem k="pgadmin6" js={this.gotoApp.bind (this, 'pgaacc?action=passwd')} t={S.mc ('Change account password')} />
+		      <NMItem k="pgadmin7" js={this.gotoApp.bind (this, 'pgarealm?action=list')} t={S.mc ('List realms')} />
+		      <NMItem k="pgadmin8" js={this.gotoApp.bind (this, 'pgarealm?action=add')} t={S.mc ('Add realm')} />
+		      <NMItem k="pgadmin9" js={this.gotoApp.bind (this, 'pgarealm?action=mod')} t={S.mc ('Modify realm')} />
+		      <NMItem k="pgadmin10" js={this.gotoApp.bind (this, 'pgarealm?action=del')} t={S.mc ('Remove realm')} />
+		    </NMDropdown>
 
 		  </ul>
 
@@ -358,71 +180,20 @@ export var NMMenu = React.createClass ({
 			{S.mc ('Not connected')}
 		      </p>
 		    </li>
-		    <li className={"dropdown" + this.showIf ('logged')}
-			key="user"
-			>
-		      <a href="#" className="dropdown-toggle"
-				data-toggle="dropdown"
-				role="button"
-				aria-haspopup="true"
-				aria-expanded="false"
-				>
-			<span className="glyphicon glyphicon-user" />
-			&nbsp; {this.context.nm.user}
-			<span className="caret" />
-		      </a>
-		      <ul className="dropdown-menu">
-			<li key="user1">
-			  <a href="profile.html">{S.mc ('Profile')}</a>
-			</li>
-			<li key="user2">
-			  <a href="sessions.html">{S.mc ('Sessions')}</a>
-			</li>
-			<li key="user3" role="separator"
-			    className={"divider" + this.showIf ('admin')}
-			    />
-			<li key="user4" className={this.showIf ('admin')}>
-			  <a href="sudo.html">{S.mc ('Sudo')}</a>
-			</li>
-			<li key="user5" className={this.showIf ('setuid')}>
-			  <a href="sudo.html">{S.mc ('Back to my id')}</a>
-			</li>
-			<li key="user6" role="separator" className="divider"
-			    />
-			<li key="user7">
-			  <a href="#">
-			    <p onClick={S.disconnect}>
-			      {S.mc ('Disconnect')}
-			    </p>
-			  </a>
-			</li>
-		      </ul>
-		    </li>
+		    <NMDropdown k="user" t={this.context.nm.user} i="glyphicon glyphicon-user" show={this.showIf ('logged')}>
+			<NMItem k="user1" js={this.gotoApp.bind (this, 'profile')} t={S.mc ('Profile')} />
+			<NMItem k="user2" js={this.gotoApp.bind (this, 'sessions')} t={S.mc ('Sessions')} />
+			<NMISep k="user3" show={this.showIf ('admin')} />
+			<NMItem k="user4" js={this.gotoApp.bind (this, 'sudo')} t={S.mc ('Sudo')} show={this.showIf ('admin')} />
+			<NMItem k="user5" js={this.gotoApp.bind (this, 'sudoback')} t={S.mc ('Back to my id')} show={this.showIf ('setuid')} />
+			<NMISep k="user6" />
+			<NMItem k="user7" js={S.disconnect} t={S.mc ('Disconnect')} />
+		    </NMDropdown>
 
-		    <li className="dropdown">
-		      <a href="#"
-			  className="dropdown-toggle"
-			  data-toggle="dropdown"
-			  role="button"
-			  aria-haspopup="true"
-			  aria-expanded="false"
-			  >
-			{S.mc ('[en]')}
-			<span className="caret" />
-		      </a>
-		      <ul className="dropdown-menu">
-			<li key="lang-en">
-			  <a href="#">
-			    <p onClick={S.changeLang.bind (this, 'en')}>[en]</p>
-			  </a>
-			</li>
-			<li key="lang-fr">
-			  <a href="#">
-			    <p onClick={S.changeLang.bind (this, 'fr')}>[fr]</p>
-			  </a>
-			</li>
-		      </ul>
-		    </li>
+		    <NMDropdown k="lang" t={S.mc ('[en]')}>
+			<NMItem k="lang-en" js={S.changeLang.bind (this, 'en')} t="[en]" />
+			<NMItem k="lang-fr" js={S.changeLang.bind (this, 'fr')} t="[fr]" />
+		    </NMDropdown>
 
 		  </ul>
 		</div>
@@ -438,6 +209,82 @@ export var NMMenu = React.createClass ({
 	    </div>
 	  </div>
 	) ;
+    }
+}) ;
+
+
+/*
+ * Short-hand to help defining menu dropdowns
+ * props:
+ * - k: key
+ * - show (optional): 'hidden' or nothing
+ * - t: text for this menu
+ # - i (optional): icon
+ */
+
+var NMDropdown = React.createClass ({
+    render: function () {
+	var icon = 'hidden' ;
+	if (this.props.i) {
+	    icon = this.props.i
+	}
+	return (
+		<li className={"dropdown" + this.props.show}>
+		  <a href="#"
+		      className="dropdown-toggle"
+		      data-toggle="dropdown"
+		      role="button"
+		      aria-haspopup="true"
+		      aria-expanded="false"
+		      >
+		    <span className={icon}>&nbsp;</span>
+		    {this.props.t}
+		    <span className="caret" />
+		  </a>
+		  <ul className="dropdown-menu">
+		    {this.props.children}
+		  </ul>
+		</li>
+	    ) ;
+    }
+}) ;
+
+/*
+ * Short-hand to help defining menu items
+ * props:
+ * - k: key
+ * - show (optional): 'hidden' or nothing
+ * - js: javascript to call when clicked
+ * - t: text for this menu item
+ */
+
+var NMItem = React.createClass ({
+    render: function () {
+	return (
+		<li key={this.props.k} className={this.props.show}>
+		  <a href="#" onClick={this.props.js}>
+		    {this.props.t}
+		  </a>
+		</li>
+	    ) ;
+    }
+}) ;
+
+/*
+ * Short-hand to help defining a menu item separator
+ * props:
+ * - k: key
+ * - show (optional): 'hidden' or nothing
+ */
+
+var NMISep = React.createClass ({
+    render: function () {
+	return (
+		<li key={this.props.k}
+		    role="separator"
+		    className={'divider ' + this.props.show}
+		    />
+	    ) ;
     }
 }) ;
 
