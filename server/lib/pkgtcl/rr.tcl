8bcb8873819afcf96d294f0c781aa5856155a82f
Pierre DAVID <pdagog@gmail.com>
2016-11-04 16:51:20 +0100

    Bring rr.tcl to the 3.0 schema

--- server/lib/pkgtcl/rr.tcl
+++ server/lib/pkgtcl/rr.tcl
@@ -9,14 +9,14 @@ package provide rr 0.1
 # access various attributes.
 #
 # Functions:
-# - read-by-id $db $idrr
+# - read-by-idname $db $idname
 #	return a RR object, which may be empty
 # - read-by-name $db $name $iddom $idview
 #	return a RR object, which may be empty
 # - found $rr
 #	return 1 if the RR has been found, 0 if is has not been found
-# - get-idrr $rr
-#	return the idrr
+# - get-idname $rr
+#	return the id of the name
 # - get-name $rr
 #	return the RR name (without domain)
 # - get-iddom $rr
@@ -27,126 +27,131 @@ package provide rr 0.1
 #	return the FQDN of the RR
 # - get-idview $rr
 #	return the view id
+# - get-idhost $rr
+#	return the idhost of the host, or -1 if the name is not a host
 # - get-mac $rr
-#	return the MAC address or an empty string
+#	return the MAC address of the host or an empty string
 # - get-iddhcpprof $rr
-#	return the DHCP profile id, or 0 if none
+#	return the DHCP profile id of the host, or 0 if none
 # - get-dhcpprof $rr
-#	return the DHCP profile name, or an empty string
+#	return the DHCP profile name of the host, or an empty string
 #			(XXX WARNING OLD code was returing "No profile")
 # - get-idhinfo $rr
-#	return the idhinfo
+#	return the idhinf of the hosto
 # - get-hinfo $rr
-#	return the hinfo text
+#	return the hinfo tex of the hostt
 # - get-sendsmtp $rr
 #	return 1 if host has the right to emit with non auth SMTP, or 0
-# - get-ttl $rr
-#	return the ttl (0...n) for all its IP addresses
+# - get-ttlhost $rr
+#	return the ttl (0...n) of the host for all its IP addresses
 # - get-comment $rr
 #	return the comment
 # - get-respname $rr
-#	return the name of the responsible person
+#	return the name of the responsible person for the host
 # - get-respmail $rr
-#	return the mail address of the responsible person
-# - get-idcor $rr
-#       return the id of user who has done the last modification
-# - get-date $rr
-#       return the date of last modification (SQL timestamp format)
+#	return the mail address of the responsible person for the host
 # - get-ip $rr
-#	return the list of all IP addresses {addr addr...}
+#	return the list of all IP addresses {addr addr...} of the host
 #			(XXX OLD {{idview addr} ...})
-# - get-mx $rr
-#	return the list of target MX for this name {{prio idrr} {prio idrr}...}
+# - get-mxhost $rr
+#	return the list of MX target hosts for this name under the format
+#	{{prio idrr ttl} {prio idrr ttl}...}
 #			(XXX OLD {{idview prio idrr} {idview prio idrr} ...})
-# - get-mxtarg $rr
-#	return the list of MX which target this host {idrr idrr...}
+# - get-mxname $rr
+#	return the list of MX names which target this host {idrr idrr...}
 # - get-cname $rr
 #	return the idrr of the referenced host if $rr is an alias
+# - get-ttlcname $rr
+#	return the TTL associated to the alias or -1
 # - get-aliases $rr
-#	return the list of idrr for aliases to this host
+#	return the list of names which are aliases of this host
 # - get-mboxhost $rr
-#	return the idrr of the mailbox host for this mail address
+#	return the idhost of the mailbox host for this mail address
 # - get-mailaddr $rr
-#	return the list of idrr of mail addresses if $rr is a mbox host
+#	return the list of idname of mail addresses if $rr is a mbox host
 #			(XXX OLD {{idview idmailaddr idviewmailaddr} ...})
 #
 
 namespace eval ::rr {
     namespace export read-by-id read-by-name \
 		    found \
+		    is-other-than-host \
 		    get-mx get-fqdn
 
     proc read-by-name {db name iddom idview} {
 	set qname [pg_quote $name]
-	set where "rr.name = $qname
-			AND rr.iddom = $iddom
-			AND rr.idview = $idview"
+	set where "n.name = $qname
+			AND n.iddom = $iddom
+			AND n.idview = $idview"
 	return [Read-where $db $where]
     }
 
-    proc read-by-id {db idrr} {
-	set where "rr.idrr = $idrr"
+    proc read-by-idname {db idname} {
+	set where "n.idname = $idname"
 	return [Read-where $db $where]
     }
 
     proc Read-where {db where} {
 	set found 0
 	set sql "SELECT row_to_json (j) FROM (
-		    SELECT rr.idrr, rr.name AS name,
-			    rr.iddom AS iddom, domain.name AS domain,
-			    rr.idview AS idview,
-			    COALESCE (CAST (rr.mac AS text), '') AS mac,
-			    COALESCE (rr.iddhcpprof, 0) AS iddhcpprof,
-			    COALESCE (dhcpprofile.name, '') AS dhcpprof,
-			    rr.idhinfo, hinfo.name AS hinfo,
-			    rr.sendsmtp, rr.ttl,
-			    rr.comment, rr.respname, rr.respmail,
-			    rr.idcor, rr.date,
-			    COALESCE (sreq_ip.ip, '{}') AS ip,
-			    COALESCE (sreq_mx.mx, '{}') AS mx,
-			    COALESCE (sreq_mxtarg.mxtarg, '{}') AS mxtarg,
-			    COALESCE (CAST (cn.cname AS text), '') AS cname,
-			    COALESCE (sreq_aliases.aliases, '{}') AS aliases,
-			    COALESCE (CAST (mail_role.mboxhost AS text), '')
-				    AS mboxhost,
-			    COALESCE (sreq_mailaddr.mailaddr, '{}') AS mailaddr
-			FROM dns.rr
-			    INNER JOIN dns.domain USING (iddom)
-			    INNER JOIN dns.hinfo USING (idhinfo)
-			    LEFT OUTER JOIN dns.dhcpprofile USING (iddhcpprof)
-			    LEFT OUTER JOIN dns.rr_cname cn USING (idrr)
-			    LEFT OUTER JOIN dns.mail_role ON (mailaddr = idrr)
-			    , LATERAL (
-				    SELECT array_agg (addr) AS ip
-					FROM dns.rr_ip
-					WHERE rr_ip.idrr = rr.idrr
-				) AS sreq_ip
-			    , LATERAL (
-				    SELECT array_agg (json_build_object (
-							'idmx', rr_mx.mx,
-							'prio', rr_mx.prio
-					    )) AS mx
-					FROM dns.rr_mx
-					WHERE rr_mx.idrr = rr.idrr
-				) AS sreq_mx
-			    , LATERAL (
-				SELECT array_agg (rr_mx.idrr) AS mxtarg
-				    FROM dns.rr_mx
-				    WHERE rr_mx.mx = rr.idrr
-				) AS sreq_mxtarg
-			    , LATERAL (
-				    SELECT array_agg (rr_cname.idrr) AS aliases
-					FROM dns.rr_cname
-					WHERE rr_cname.cname = rr.idrr
-				    ) AS sreq_aliases
-			    , LATERAL (
-				    SELECT array_agg (mailaddr) AS mailaddr
-					FROM dns.mail_role
-					WHERE mail_role.mboxhost = rr.idrr
-				    ) AS sreq_mailaddr
-			WHERE $where
-		    ) AS j
-		    "
+	    SELECT n.idname, n.name AS name,
+		    n.iddom AS iddom, domain.name AS domain,
+		    n.idview AS idview,
+		    COALESCE (h.idhost, -1) AS idhost,
+		    COALESCE (CAST (h.mac AS text), '') AS mac,
+		    COALESCE (h.iddhcpprof, 0) AS iddhcpprof,
+		    COALESCE (dhcpprofile.name, '') AS dhcpprof,
+		    h.idhinfo, hinfo.name AS hinfo,
+		    h.sendsmtp, h.ttl AS ttlhost,
+		    h.comment, h.respname, h.respmail,
+		    COALESCE (sreq_ip.ip, '{}') AS ip,
+		    COALESCE (sreq_mxhost.mxhost, '{}') AS mxhost,
+		    COALESCE (sreq_mxname.mxname, '{}') AS mxname,
+		    COALESCE (CAST (a.idhost AS text), '') AS cname,
+		    COALESCE (a.ttl, -1) AS ttlcname,
+		    COALESCE (sreq_aliases.aliases, '{}') AS aliases,
+		    COALESCE (CAST (mailrole.mboxhost AS text), '') AS mboxhost,
+		    COALESCE (mailrole.ttl) AS ttlmboxhost,
+		    COALESCE (sreq_mailaddr.mailaddr, '{}') AS mailaddr
+		FROM dns.name n
+		    INNER JOIN dns.domain USING (iddom)
+		    LEFT OUTER JOIN dns.host h USING (idname)
+		    LEFT OUTER JOIN dns.hinfo USING (idhinfo)
+		    LEFT OUTER JOIN dns.dhcpprofile USING (iddhcpprof)
+		    LEFT OUTER JOIN dns.alias a USING (idname)
+		    LEFT OUTER JOIN dns.mailrole ON (mailaddr = n.idname)
+		    , LATERAL (
+			    SELECT array_agg (addr) AS ip
+				FROM dns.addr
+				WHERE addr.idhost = h.idhost
+			) AS sreq_ip
+		    , LATERAL (
+			    SELECT array_agg (json_build_object (
+						'idhost', mx.idhost,
+						'prio', mx.prio,
+						'ttl', mx.ttl
+				    )) AS mxhost
+				FROM dns.mx
+				WHERE mx.idname = n.idname
+			) AS sreq_mxhost
+		    , LATERAL (
+			SELECT array_agg (mx.idname) AS mxname
+			    FROM dns.mx
+			    WHERE mx.idhost = h.idhost
+			) AS sreq_mxname
+		    , LATERAL (
+			    SELECT array_agg (alias.idname) AS aliases
+				FROM dns.alias
+				WHERE alias.idhost = h.idhost
+			    ) AS sreq_aliases
+		    , LATERAL (
+			    SELECT array_agg (mailaddr) AS mailaddr
+				FROM dns.mailrole
+				WHERE mailrole.mboxhost = h.idhost
+			    ) AS sreq_mailaddr
+		WHERE $where
+	    ) AS j
+	"
 
 	set rr [dict create]
 	$db exec $sql tab {
@@ -160,16 +165,24 @@ namespace eval ::rr {
 	return [expr [dict size $rr] > 0]
     }
 
+    proc is-other-than-host {rr} {
+	set mb [get-mboxhost $rr]
+	set mx [get-mx $rr]
+	set r 0
+	if {$mb ne "" || [llength $mx] > 0} then {
+	    set r 1
+	}
+	return $r
+    }
+
     # Define all getters, except mx
-    foreach key {idrr name iddom domain idview mac
-		    iddhcpprof dhcpprof
-		    idhinfo hinfo
-		    sendsmtp ttl
+    foreach key {idname name iddom domain idview
+		    idhost mac iddhcpprof dhcpprof idhinfo hinfo
+		    sendsmtp ttlhost
 		    comment respname respmail
-		    idcor date
 		    ip
-		    mxtarg
-		    cname
+		    mxname
+		    cname ttlcname
 		    aliases
 		    mboxhost mailaddr} {
 	namespace export get-$key
@@ -177,10 +190,13 @@ namespace eval ::rr {
 	proc get-$key {rr} "return \[dict get \$rr $key]"
     }
 
-    proc get-mx {rr} {
+    proc get-mxhost {rr} {
 	set r {}
-	foreach j [dict get $rr "mx"] {
-	    lappend r [list [dict get $j "prio"] [dict get $j "idmx"]]
+	foreach j [dict get $rr "mxhost"] {
+	    lappend r [list [dict get $j "prio"] \
+			    [dict get $j "idhost"] \
+			    [dict get $j "ttl"] \
+			    ]
 	}
 	return $r
     }
