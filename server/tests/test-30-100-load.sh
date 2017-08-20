d3bd855b01171bfb9e77621915d64d4aa1159c57
Pierre DAVID <pdagog@gmail.com>
2017-08-20 19:21:56 +0200

    Specify distinct users for tests

--- server/tests/test-30-100-load.sh
+++ server/tests/test-30-100-load.sh
@@ -21,9 +21,33 @@ PGDATABASE=$(getconf dnsdbname)
 PGUSER=$(getconf dnsdbuser)
 PGPASSWORD=$(getconf dnsdbpassword)
 
+PGOPT="--quiet --no-psqlrc"
+
 export PGHOST PGPORT PGDATABASE PGUSER PGPASSWORD
 dropdb --if-exists $PGDATABASE \
     && createdb $PGDATABASE \
-    && psql -f $DUMPFILE \
-    && psql \
-	-c "INSERT INTO global.utmp (idcor, token, api) VALUES (1, 'bla', 1)"
+    && psql $PGOPT -f $DUMPFILE \
+    && psql $PGOPT -c "
+	INSERT INTO global.utmp (idcor, token, api) VALUES (1, 't1-wheel', 1) ;
+	INSERT INTO global.utmp (idcor, token, api) VALUES (2, 't2-simple', 1) ;
+	INSERT INTO global.utmp (idcor, token, api) VALUES (3, 't3-genz', 1) ;
+	INSERT INTO global.utmp (idcor, token, api) VALUES (4, 't4-mac', 1) ;
+	INSERT INTO global.utmp (idcor, token, api) VALUES (5, 't5-ttl', 1) ;
+	INSERT INTO global.utmp (idcor, token, api) VALUES (6, 't6-smtp', 1) ;
+	INSERT INTO global.utmp (idcor, token, api) VALUES (7, 't7-admin', 1) ;
+	INSERT INTO global.utmp (idcor, token, api) VALUES (8, 't8-abset', 1) ;
+	"
+
+r=$?
+if [ $r = 0 ]
+then
+    echo "ok 10 Load 3.0 dump"
+    echo
+else
+    echo "not ok 10 Load 3.0 dump"
+    echo "	exit code=$r"
+    echo
+fi
+
+exit $r
+
